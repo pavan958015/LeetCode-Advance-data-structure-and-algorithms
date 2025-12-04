95fe6240245600414bd0c066c611cc6677065fe0
Pavan Soni <pavansoni210129@gmail.com>
2025-12-04 10:38:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (24.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,12 +1,36 @@
 class Solution {
-    public int climbStairs(int n) {
-        if(n<=1) return 1;
+    // Bottom Up Approch
+    // public int climbStairs(int n) {
+    //     if(n<=1) return 1;
+    //     int dp[]=new int[n+1];
+    //     dp[0]=1;
+    //     dp[1]=1;
+    //     for(int i=2;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2];
+    //     }
+    //     return dp[n];
+    // }
+    
+    // Time Limit Exceed
+    // Using Recursion
+    // public int climbStairs(int n){
+    //     if(n<=1)
+    //         return 1;
+    //     return climbStairs(n-1)+climbStairs(n-2);
+    // }
+
+    public int climbStairs(int n){
         int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
+    public int helper(int n,int[] dp){
+        if(n<=1){
+            return 1;
         }
-        return dp[n];
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
     }
 }
