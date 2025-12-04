529ebc75763bc1f4ddb11a7e1b3b3548b4a9e883
Pavan Soni <pavansoni210129@gmail.com>
2025-12-04 10:41:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (13.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,15 @@
 class Solution {
     // Bottom Up Approch
-    // public int climbStairs(int n) {
-    //     if(n<=1) return 1;
-    //     int dp[]=new int[n+1];
-    //     dp[0]=1;
-    //     dp[1]=1;
-    //     for(int i=2;i<=n;i++){
-    //         dp[i]=dp[i-1]+dp[i-2];
-    //     }
-    //     return dp[n];
-    // }
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     
     // Time Limit Exceed
     // Using Recursion
@@ -19,18 +19,22 @@ class Solution {
     //     return climbStairs(n-1)+climbStairs(n-2);
     // }
 
-    public int climbStairs(int n){
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(n,dp);
-    }
-    public int helper(int n,int[] dp){
-        if(n<=1){
-            return 1;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
-        }
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
-    }
+    // Top Down + Memorization
+    // public int climbStairs(int n){
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //     return helper(n,dp);
+    // }
+    // public int helper(int n,int[] dp){
+    //     if(n<=1){
+    //         return 1;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    // }
+
+    // Space Optimization
+
 }
