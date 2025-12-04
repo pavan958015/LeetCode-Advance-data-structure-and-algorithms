b5298537e6913896742ca7c6dd48f335af1af324
Pavan Soni <pavansoni210129@gmail.com>
2025-12-04 10:44:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (35.63%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,9 +3,10 @@ class Solution {
     public int climbStairs(int n) {
         if(n<=1) return 1;
         int dp[]=new int[n+1];
-        dp[0]=1;
+        dp[0]=0;
         dp[1]=1;
-        for(int i=2;i<=n;i++){
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
@@ -37,4 +38,9 @@ class Solution {
 
     // Space Optimization
 
+    int a=0;
+    int b=1;
+    
+
+    
 }
