7a0cba6d78c7cee82ad95a27353a0927fd718aa1
Pavan Soni <pavansoni210129@gmail.com>
2025-12-09 10:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (53.86%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,33 @@
 class Solution {
-    public int rob(int[] nums) {
-        int dp[]=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return helper(nums.length-1,nums,dp);
-    }
-    public int helper(int n,int[] nums,int[] dp){
-        if(n<0) return 0;
-        if(dp[n]!=-1){
-            return dp[n];
+    // public int rob(int[] nums) {
+    //     int dp[]=new int[nums.length];
+    //     Arrays.fill(dp,-1);
+    //     return helper(nums.length-1,nums,dp);
+    // }
+    // public int helper(int n,int[] nums,int[] dp){
+    //     if(n<0) return 0;
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+    //     int accept=nums[n]+helper(n-2,nums,dp);
+    //     int skip=helper(n-1,nums,dp);
+    //     dp[n]=Math.max(accept,skip);
+    //     return dp[n];
+    // }
+
+    public int rob(int[] nums){
+        int n=nums.length;
+        int dp[]=new int[nums.length+1];
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            int accept=nums[i-1]+dp[i-2];
+            int skip=dp[i-1];
+            dp[i]=Math.max(accept,skip);
         }
-        int accept=nums[n]+helper(n-2,nums,dp);
-        int skip=helper(n-1,nums,dp);
-        dp[n]=Math.max(accept,skip);
         return dp[n];
+
     }
 }
