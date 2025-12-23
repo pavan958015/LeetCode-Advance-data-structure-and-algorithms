51b66e04dc0c4c93704354daf2562468d796f74b
Pavan Soni <pavansoni210129@gmail.com>
2025-12-23 09:40:58 +0530

    Time: 37 ms (5.38%) | Memory: 54 MB (41.24%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m=text1.length();
+        int n=text2.length();
+        int dp[][] = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return solve(m,n,text1,text2,dp);
+    }
+    // public static int solve(int m, int n, String str1, String str2) {
+    //     // BASE;
+    //     if (m == 0 || n == 0) {
+    //         return 0;
+    //     }
+
+    //     // TWO OPTION
+    //     // IF MATCHED
+    //     if (str1.charAt(m - 1) == str2.charAt(n - 1)) {
+    //         return 1 + solve(m - 1, n - 1, str1, str2);
+    //     } else {
+    //         // NOT MATCH
+    //         return Math.max(0 + solve(m - 1, n, str1, str2), 
+    //                         0 + solve(m, n - 1, str1, str2));
+    //     }
+    // }
+
+
+    // dp approach
+    public static int solve(int m, int n, String s, String t, int[][] dp) {
+        // base 
+        if(m==0 || n==0){
+            return 0;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        if(s.charAt(m-1) == t.charAt(n-1)){
+            dp[m][n] = 1 + solve(m-1, n-1, s, t, dp);
+            return dp[m][n];
+        } else {
+            dp[m][n] = Math.max(0 + solve(m-1, n, s, t, dp), 0 + solve(m, n-1, s, t, dp));
+            return dp[m][n];
+        }
+    }
+
+
+}
