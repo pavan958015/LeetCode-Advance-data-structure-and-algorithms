7533e298793e11e9f4c2c5990d3eb03311c6986b
Pavan Soni <pavansoni210129@gmail.com>
2025-12-24 23:24:26 +0530

    Time: 17 ms (92.33%) | Memory: 43.1 MB (95.20%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,16 +1,19 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int m=text1.length();
-        int n=text2.length();
-        int dp[][] = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        // return solve(m,n,text1,text2,dp);
-        return solve(m,n,text1,text2);
-    }
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     int m=text1.length();
+    //     int n=text2.length();
+    //     int dp[][] = new int[m + 1][n + 1];
+    //     for (int i = 0; i <= m; i++) {
+    //         for (int j = 0; j <= n; j++) {
+    //             dp[i][j] = -1;
+    //         }
+    //     }
+    //     // return solve(m,n,text1,text2,dp);
+    //     return solve(m,n,text1,text2);
+    // }
+
+    // Using Recursion
+
     // public static int solve(int m, int n, String str1, String str2) {
     //     // BASE;
     //     if (m == 0 || n == 0) {
@@ -49,23 +52,53 @@ class Solution {
 
 
     // tabulation
-    public static int solve(int m,int n,String s1,String s2){
-        int tab[][]=new int[m+1][n+1];
+    // public static int solve(int m,int n,String s1,String s2){
+    //     int tab[][]=new int[m+1][n+1];
 
-        for(int i=0;i<=m;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0 || j==0){
-                    tab[i][j]=0;
-                }
-                 else if(s1.charAt(i-1)==s2.charAt(j-1)){
-                    tab[i][j]=tab[i-1][j-1]+1;
+    //     for(int i=0;i<=m;i++){
+    //         for(int j=0;j<=n;j++){
+    //             if(i==0 || j==0){
+    //                 tab[i][j]=0;
+    //             }
+    //              else if(s1.charAt(i-1)==s2.charAt(j-1)){
+    //                 tab[i][j]=tab[i-1][j-1]+1;
+    //             }else{
+    //                 tab[i][j]=Math.max(tab[i-1][j],tab[i][j-1]);
+    //             }
+    //         }
+    //     }
+    //     return tab[m][n];
+    // }
+
+
+
+    // Space Optimization
+
+    public int longestCommonSubsequence(String text1, String text2){
+        int n=text1.length(); int m=text2.length();
+        
+        int prev[]=new int[m+1];
+        int cur[]=new int[m+1];
+
+        for(int j=0;j<=m;j++){
+            prev[j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    cur[j]=1+prev[j-1];
                 }else{
-                    tab[i][j]=Math.max(tab[i-1][j],tab[i][j-1]);
+                    cur[j]=Math.max(prev[j],cur[j-1]);
                 }
             }
-        }
-        return tab[m][n];
-    }
+            // swap
+            int[] temp = prev;
+            prev = cur;
+            cur = temp;
 
+        }
+    return prev[m];
+
+    }
 
 }
