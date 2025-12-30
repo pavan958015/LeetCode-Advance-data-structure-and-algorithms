764bc54bb435e109e5c80777cd7e7da5a628c13e
Pavan Soni <pavansoni210129@gmail.com>
2025-12-31 05:04:28 +0530

    Time: 2 ms (99.87%) | Memory: 43.3 MB (63.28%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
+
+        // Edge cases
+        if (Math.abs(target) > totalSum) return 0;
+        if ((totalSum + target) % 2 != 0) return 0;
+
+        int sum = (totalSum + target) / 2;
+
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = sum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[sum];
+    }
+}
