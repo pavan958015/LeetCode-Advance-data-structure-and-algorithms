e0c16f596ca46d88799e702f78f3f542326c4879
Pavan Soni <pavansoni210129@gmail.com>
2026-01-08 10:32:23 +0530

    Time: 83 ms (24.84%) | Memory: 237.4 MB (5.65%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,12 +1,17 @@
 class Solution {
     public  int lengthOfLIS(int[] nums) {
-        int dp[][]=new int[nums.length][nums.length+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return solve(0, -1, nums,dp);
+       
+        int dp[][]=new int[nums.length+1][nums.length+1];
+        
+        return tabulation(nums.length,nums,dp);
+
+        // int dp[][]=new int[nums.length][nums.length+1];
+        // for(int i=0;i<dp.length;i++){
+        //     for(int j=0;j<dp[0].length;j++){
+        //         dp[i][j]=-1;
+        //     }
+        // }
+        // return solve(0, -1, nums,dp);
     }
 
     // public static int solve(int idx, int past, int[] nums) {
@@ -27,22 +32,39 @@ class Solution {
     // }
 
     // dp 
-    public static int solve(int idx, int past, int[] nums,int dp[][]) {
-        // base;
-        if (idx == nums.length)
-            return 0;
-        if(dp[idx][past+1]!=-1){
-            return dp[idx][past+1];
-        }
-        // take;
-        int take = 0;
-            if (past == -1 || nums[idx] > nums[past]){
-            take = 1 + solve(idx + 1, idx, nums,dp);
-        }
-        // not take;
-        int notTake = solve(idx + 1, past, nums,dp);
+    // public static int solve(int idx, int past, int[] nums,int dp[][]) {
+    //     // base;
+    //     if (idx == nums.length)
+    //         return 0;
+    //     if(dp[idx][past+1]!=-1){
+    //         return dp[idx][past+1];
+    //     }
+    //     // take;
+    //     int take = 0;
+    //         if (past == -1 || nums[idx] > nums[past]){
+    //         take = 1 + solve(idx + 1, idx, nums,dp);
+    //     }
+    //     // not take;
+    //     int notTake = solve(idx + 1, past, nums,dp);
 
-        return dp[idx][past+1]=Math.max(take, notTake);
+    //     return dp[idx][past+1]=Math.max(take, notTake);
+    // }
+
+
+    public static int tabulation(int n,int nums[],int dp[][]){
+        for(int i=n-1;i>=0;i--){
+            for(int past=i-1;past>=-1;past--){
+                int take = 0;
+                if (past == -1 || nums[i] > nums[past]){
+                take = 1 + dp[i+1][i+1];
+                }
+                // not take;
+                int notTake = dp[i+1][past+1];
+
+                 dp[i][past+1]=Math.max(take, notTake);
+            }
+        }
+        return dp[0][0];
     }
 
 }
