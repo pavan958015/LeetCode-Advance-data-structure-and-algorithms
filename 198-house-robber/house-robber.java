274334043f9c503a5a34b20b82d4dbce1f46bc28
Pavan Soni <pavansoni210129@gmail.com>
2026-01-11 21:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (21.02%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -15,19 +15,36 @@ class Solution {
     //     return dp[n];
     // }
 
+    // public int rob(int[] nums){
+    //     int n=nums.length;
+    //     int dp[]=new int[nums.length+1];
+    //     if(n==0) return 0;
+    //     if(n==1) return nums[0];
+    //     dp[0]=0;
+    //     dp[1]=nums[0];
+    //     for(int i=2;i<=n;i++){
+    //         int accept=nums[i-1]+dp[i-2];
+    //         int skip=dp[i-1];
+    //         dp[i]=Math.max(accept,skip);
+    //     }
+    //     return dp[n];
+    // }
+
+
     public int rob(int[] nums){
         int n=nums.length;
-        int dp[]=new int[nums.length+1];
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            int accept=nums[i-1]+dp[i-2];
-            int skip=dp[i-1];
-            dp[i]=Math.max(accept,skip);
-        }
-        return dp[n];
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int accept=nums[i];
+            if(i>1)
+                accept+=prev2;
+            int skip=prev;
+            int cur=Math.max(accept,skip);
 
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
     }
 }
