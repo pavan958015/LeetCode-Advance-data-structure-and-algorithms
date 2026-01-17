9c2b4b0341d2ecb876642b5c365c7c2593b5b974
Pavan Soni <pavansoni210129@gmail.com>
2026-01-17 12:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (7.21%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,10 @@
 class Solution {
     
+
+
+    //1.
+
+
      public static int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
         for (int i = 0; i < m; i++) {
@@ -25,4 +30,37 @@ class Solution {
         return dp[m][n];
 
     }
+
+
+
+    //2.
+
+
+    // using Grid Tp Techniuqe;
+
+    // public static int unique(int m, int n) {
+    //     int dp[][] = new int[m][n];
+    //     for (int i = 0; i < m; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             if (i == 0 && j == 0) {
+    //                 dp[i][j] = 1;
+    //             } else {
+    //                 int up = 0;
+    //                 int left = 0;
+    //                 if (i > 0) {
+    //                     up = dp[i - 1][j];
+    //                 }
+    //                 if (j > 0) {
+    //                     left = dp[i][j - 1];
+    //                 }
+    //                 dp[i][j] = up + left;
+    //             }
+    //         }
+    //     }
+    //     return dp[m - 1][n - 1];
+    // }
+
+
+
+    //3.
 }
