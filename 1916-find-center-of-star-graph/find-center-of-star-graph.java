73da554f31db66d3a90614eacea4783e6129cb00
Pavan Soni <pavansoni210129@gmail.com>
2026-02-08 03:16:52 +0530

    Time: 0 ms (100.00%) | Memory: 110.9 MB (76.14%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.java
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -1,53 +1,72 @@
 class Solution {
+    // using 2d matrix
     // public int findCenter(int[][] edges) {
-    //     int nodes = edges.length + 1;
+    //     // find nodes
+    //     int nodes=edges.length+1;
+    //     // make 2d matrix;
     //     int[][] matrix=new int[nodes+1][nodes+1];
-    //     for(int[] e:edges){
-    //         int u=e[0];
-    //         int v=e[1];
-    //         matrix[u][v]=1;
-    //         matrix[v][u]=1;
+    //     // find edges and store one 
+    //     for(int i=0;i<edges.length;i++){
+    //         for(int e[]:edges){
+    //             int u=e[0];
+    //             int v=e[1];
+    //             matrix[u][v]=1;
+    //             matrix[v][u]=1;
 
+    //         }
     //     }
+    //     // count each node which has n-1 edges;
     //     for(int i=0;i<=nodes;i++){
     //         int count=0;
     //         for(int j=0;j<=nodes;j++){
-    //             if(matrix[i][j]==1){
-    //                 count++;
-    //             }
+    //             if(matrix[i][j]==1) count++;
     //         }
-    //         if(count==nodes-1){
-    //             return i;
-    //         }
+    //         if(count==nodes-1) return i;
     //     }
     //     return -1;
     // }
 
 
-    //  public int findCenter(int[][] edges){
-    //        int m=edges.length;
-    //        int n=m+1;
-    //        List<List<Integer>> adj=new ArrayList<>();
-    //        for(int i=0;i<m;i++){
-    //             int u=edges[i][0];
-    //             int v=edges[i][1];
+    // using adjancy List
 
-                
-    //        }
-    //        for(int i=1;i<=n;i++){
-    //             if(adj.get(i).size()==n-1){
-    //                 return i;
-    //             }
-    //        }
-    //        return -1;
+    //  public int findCenter(int[][] edges) {
+    //     int nodes=edges.length+1;
+    //     List<List<Integer>> adj=new ArrayList<>();
+    //     for(int i=0;i<=nodes;i++){
+    //         adj.add(new ArrayList<>());
+    //     }
+    //         for(int e[]:edges){
+    //             int u=e[0];
+    //             int v=e[1];
+    //             adj.get(u).add(v);
+    //             adj.get(v).add(u);
+    //         }
+        
+    //     // for(int i=0;i<adj.size();i++){
+    //     //     System.out.print(i+"->");
+    //     //     for(int x:adj.get(i)){
+    //     //         System.out.print(x+" ");
+    //     //     }
+    //     //     System.out.println();
+    //     // }
+
+    //     for(int i=0;i<=nodes;i++){
+    //         if(adj.get(i).size()==nodes-1) return i;
+    //     }
+    //     return -1;
     //  }
 
-    public int findCenter(int[][] edges){
+    // using optimal approach -> common value in each edges
+
+    public int findCenter(int[][] edges) {
         int a=edges[0][0];
         int b=edges[0][1];
         int c=edges[1][0];
         int d=edges[1][1];
+
+        // check a->c || a->d
         if(a==c || a==d) return a;
-        return b;
+        else return b;
+
     }
 }
