fa985cd1105957d565008e5f6ea615ddbfd40c52
Pavan Soni <pavansoni210129@gmail.com>
2026-02-07 21:03:04 +0530

    Time: 123 ms (55.13%) | Memory: 276.3 MB (74.02%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,31 +1,42 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
         }
-        for (int e[] : edges) {
+        for(int i=0;i<edges.length;i++){
             
-            graph.get(e[0]).add(e[1]);
-            graph.get(e[1]).add(e[0]);
-
-        }
-        boolean vis[] = new boolean[n];
-        return bfs(graph, source, destination, vis);
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        } 
+        boolean vis[]=new boolean[n];
+        return usingDFS(adj,source,destination,vis);
     }
+    // public static boolean usingBFS(List<List<Integer>> adj, int source, int destination,boolean[] vis){
+    //     if(source==destination) return true;
 
-    public static boolean bfs(List<List<Integer>> graph, int source, int destination, boolean[] vis) {
-        Queue<Integer> q = new LinkedList<>();
-        vis[source] = true;
-        q.add(source);
-        while (!q.isEmpty()) {
-            int front = q.poll();
-            if (front == destination) return true;
-            for (int a : graph.get(front)) {
-                if (!vis[a]) {
-                    vis[a] = true;
-                    q.add(a);
-                }
+    //     Queue<Integer> q=new LinkedList<>();
+    //     vis[source]=true;
+    //     q.add(source);
+    //     while(!q.isEmpty()){
+    //         int node =q.poll();
+    //         for(int x:adj.get(source)){
+    //             if(x==destination) return true;
+    //             if(!vis[x]){
+    //                 q.add(x);
+    //                 vis[x]=true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    public static boolean usingDFS(List<List<Integer>> adj,int source,int destination,boolean[] vis){
+        if(source==destination) return true;
+        vis[source]=true;
+        for(int node:adj.get(source)){
+            if(node==destination) return true;
+            if(!vis[node]){
+                if(usingDFS(adj,node,destination,vis)) return true;
             }
         }
         return false;
