a2784b65bd012d59395d3de5a2fca2a8f48fee88
Pavan Soni <pavansoni210129@gmail.com>
2026-02-09 06:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (73.25%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,29 +1,46 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        int provience=0;
-        boolean vis[]=new boolean[n];
-        for(int i=0;i<n;i++){
-            if(vis[i]==false){
+        int n = isConnected.length;
+        int provience = 0;
+        boolean vis[] = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == false) {
                 provience++;
-                bfs(i,isConnected,vis);
+                // bfs(i, isConnected, vis);
+                dfs(i, isConnected, vis);
+
             }
         }
         return provience;
     }
-    	public static void bfs(int source,int[][] matrix,boolean[] vis){
-			Queue<Integer> q=new LinkedList<>();
-			vis[source]=true;
-			q.add(source);
-			while(!q.isEmpty()){
-				int city=q.poll();
-				for(int i=0;i<matrix.length;i++){
-					if(matrix[city][i]==1 && !vis[i]){
-						vis[i]=true;
-						q.add(i);
-					}
-				}
-			}
-	}
+
+    // public static void bfs(int source, int[][] matrix, boolean[] vis) {
+    //     Queue<Integer> q = new LinkedList<>();
+    //     vis[source] = true;
+    //     q.add(source);
+    //     while (!q.isEmpty()) {
+    //         int city = q.poll();
+    //         for (int i = 0; i < matrix.length; i++) {
+    //             if (matrix[city][i] == 1 && !vis[i]) {
+    //                 vis[i] = true;
+    //                 q.add(i);
+    //             }
+    //         }
+    //     }
+    // }
+
+    // using dfs
+
+    public static void dfs(int source, int[][] connected, boolean[] vis) {
+        //base conditon
+        
+        vis[source] = true;
+
+        for(int i=0;i<connected.length;i++){
+            if(connected[source][i]==1 && !vis[i]){
+                dfs(i,connected,vis);
+            }
+        }
+    }
 
 }
