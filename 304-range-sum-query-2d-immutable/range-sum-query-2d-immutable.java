c0fd02225d5f5a5418132ccb52321d876c051946
Pavan Soni <pavansoni210129@gmail.com>
2026-03-09 13:43:31 +0530

    Time: 101 ms (83.37%) | Memory: 141.7 MB (75.92%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,24 +1,49 @@
 class NumMatrix {
-    int matrix[][];
+    int preCompute[][];
 
     public NumMatrix(int[][] matrix) {
-        this.matrix=matrix;
-    }
-    
-    public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            for(int j=col1;j<=col2;j++){
-                sum+=matrix[i][j];
+        int n=matrix.length;
+        int m=matrix[0].length;
+
+        preCompute= new int[n][m];
+
+        preCompute[0][0]=matrix[0][0];
+
+        for (int i = 1; i < n; i++) {
+            preCompute[i][0] = matrix[i][0] + preCompute[i - 1][0];
+        }
+        for (int j = 1; j < m; j++) {
+            preCompute[0][j] = matrix[0][j] + preCompute[0][j-1];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                preCompute[i][j] = matrix[i][j] + preCompute[i - 1][j] + preCompute[i][j - 1]
+                        - preCompute[i - 1][j - 1];
             }
         }
-        return sum;
     }
 
+    // Brute Force O(n^2);
 
     // public int sumRegion(int row1, int col1, int row2, int col2) {
-
+    //     int sum=0;
+    //     for(int i=row1;i<=row2;i++){
+    //         for(int j=col1;j<=col2;j++){
+    //             sum+=matrix[i][j];
+    //         }
+    //     }
+    //     return sum;
     // }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+       
+        
+        int up=(row1>0)?preCompute[row1-1][col2]:0;
+        int left=(col1>0)?preCompute[row2][col1-1]:0;
+        int corner=(row1>0 && col1>0)?preCompute[row1-1][col1-1]:0;
+        int ans=preCompute[row2][col2]-up-left+corner;
+        return ans;
+    }
 }
 
 /**
