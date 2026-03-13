cc5367ff87ff87c10c6410b3c1e9a29e18c23524
Pavan Soni <pavansoni210129@gmail.com>
2026-03-13 10:03:22 +0530

    Time: 318 ms (21.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 595-big-countries/big-countries.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+
+
+-- 1.
+
+-- select name , population , area 
+-- from World
+-- where population>=25000000 or area>=3000000;
+
+-- 2.
+
+select name , population ,  area 
+from world
+where area>=3000000
+
+union
+
+select name , population ,  area 
+from world
+where population>=25000000
