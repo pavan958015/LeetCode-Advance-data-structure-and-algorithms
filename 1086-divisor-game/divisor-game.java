58a21eb367f32af5408ca4c13bdbae83837ededb
Pavan Soni <pavansoni210129@gmail.com>
2026-03-14 11:02:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (84.41%) - LeetSync

--- 1086-divisor-game/divisor-game.java
+++ 1086-divisor-game/divisor-game.java
@@ -1,16 +1,4 @@
-// class Solution {
-//     public boolean divisorGame(int n) {
-//         if(n<=1) return false;
-//         for(int i = 1; i<n; i++){
-//             if(n%i == 0){
-//                 return !divisorGame(n-i);
-//             }
-//         }
-//         return false;
-//     }
-// }
-
-
+// Revison
 
 //  class Solution {
 //      public boolean divisorGame(int n) {
