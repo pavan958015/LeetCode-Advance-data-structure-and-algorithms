b2eeb21854d14cc1dd21ff1d5150ee106722eaa2
Pavan Soni <pavansoni210129@gmail.com>
2026-03-14 10:32:57 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (83.57%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,43 +1,70 @@
-class Solution {
-    // Time Limit Exceeded;
+class Solution{
+    // using recutsion  TLE
     // public int tribonacci(int n){
-    //     if(n==0) return 0;
-    //     if(n==1) return 1;
+    //     if(n<=1) return n;
     //     if(n==2) return 1;
-
     //     return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
     // }
 
-    // using Recursion
-
-    // public int tribonacci(int n){
-    //     int dp[]=new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return helper(n,dp);
-    // }
-    // public int helper(int n,int[] dp) {
-    //     if(n==0) return 0;
-    //     if(n==1) return 1;
-    //     if(n==2) return 1;
-
-    //     if(dp[n]!=-1) return dp[n];
-
-    //     return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
-    // }
-
-    // using Bottom Up
-    public int tribonacci(int n){
+    public static int tribonacci(int n){
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
         int dp[]=new int[n+1];
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 1;
+        Arrays.fill(dp,-1);
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
+        return solve(n,dp);
+    }
+    public static int solve(int n,int[]dp){
+        if(n<=1) return n;
+        if(n==2) return 1;
 
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }
-        return dp[n];
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+
     }
 }
+// class Solution {
+//     // Time Limit Exceeded;
+//     // public int tribonacci(int n){
+//     //     if(n==0) return 0;
+//     //     if(n==1) return 1;
+//     //     if(n==2) return 1;
+
+//     //     return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+//     // }
+
+//     // using Recursion
+
+//     // public int tribonacci(int n){
+//     //     int dp[]=new int[n+1];
+//     //     Arrays.fill(dp,-1);
+//     //     return helper(n,dp);
+//     // }
+//     // public int helper(int n,int[] dp) {
+//     //     if(n==0) return 0;
+//     //     if(n==1) return 1;
+//     //     if(n==2) return 1;
+
+//     //     if(dp[n]!=-1) return dp[n];
+
+//     //     return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+//     // }
+
+//     // using Bottom Up
+//     public int tribonacci(int n){
+//         int dp[]=new int[n+1];
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         if(n==2) return 1;
+//         dp[0]=0;
+//         dp[1]=1;
+//         dp[2]=1;
+
+//         for(int i=3;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+//         return dp[n];
+//     }
+// }
