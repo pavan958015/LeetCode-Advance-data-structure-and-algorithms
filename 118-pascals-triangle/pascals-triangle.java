993e5a98d04e859a5903cbc324c4a32a29f5153c
Pavan Soni <pavansoni210129@gmail.com>
2026-03-14 10:53:06 +0530

    Time: 1 ms (95.18%) | Memory: 43.7 MB (29.84%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,17 +1,35 @@
-class Solution {
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         List<List<Integer>> triangle = new ArrayList<>();
+
+//         for (int i = 0; i < numRows; i++) {
+//             List<Integer> row = new ArrayList<>();
+//             row.add(1); // first element
+
+//             for (int j = 1; j < i; j++) {
+//                 row.add(triangle.get(i - 1).get(j - 1) 
+//                         + triangle.get(i - 1).get(j));
+//             }
+
+//             if (i > 0) row.add(1); // last element
+//             triangle.add(row);
+//         }
+//         return triangle;
+//     }
+// }
+
+
+class Solution{
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            row.add(1); // first element
-
-            for (int j = 1; j < i; j++) {
-                row.add(triangle.get(i - 1).get(j - 1) 
-                        + triangle.get(i - 1).get(j));
+        List<List<Integer>> triangle=new ArrayList<>();
+        for(int i=0;i<numRows;i++){
+            List<Integer> row=new ArrayList<>();
+            row.add(1);     // first element
+            for(int j=1;j<i;j++){
+                row.add(triangle.get(i-1).get(j-1)+triangle.get(i-1).get(j));
             }
-
-            if (i > 0) row.add(1); // last element
+            if(i>0)
+                row.add(1);     // last element
             triangle.add(row);
         }
         return triangle;
