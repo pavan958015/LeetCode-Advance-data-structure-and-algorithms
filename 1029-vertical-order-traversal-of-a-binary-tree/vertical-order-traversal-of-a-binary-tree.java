5496b31d3c76addacdf5ac6f9321459b0ff417bc
Pavan Soni <pavansoni210129@gmail.com>
2026-03-17 16:13:33 +0530

    Time: 3 ms (94.77%) | Memory: 44.1 MB (62.96%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,34 @@
+class Solution {
+    Map<Integer, Map<Integer, List<Integer>>> map = new TreeMap<>();
+
+    public void dfs(TreeNode root, int row, int col) {
+        if (root == null) return;
+
+        // Initialize column
+        map.putIfAbsent(col, new TreeMap<>());
+        // Initialize row inside column
+        map.get(col).putIfAbsent(row, new ArrayList<>());
+        // Add value
+        map.get(col).get(row).add(root.val);
+
+        dfs(root.left, row + 1, col - 1);
+        dfs(root.right, row + 1, col + 1);
+    }
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        dfs(root, 0, 0);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int col:map.keySet()){
+            List<Integer> cols=new ArrayList<>();
+            for(int row:map.get(col).keySet()){
+                List<Integer> values=map.get(col).get(row);
+                Collections.sort(values);
+                cols.addAll(values);
+            }
+            ans.add(cols);
+        }
+        return ans;
+    }
+}
