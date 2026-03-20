a42c04966519d56e2ec104d00eaa098806cfabf0
Pavan Soni <pavansoni210129@gmail.com>
2026-03-20 09:32:59 +0530

    Time: 333 ms (6.56%) | Memory: 48.2 MB (6.03%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,15 +1,37 @@
 class Solution {
+    // public boolean canJump(int[] nums) {
+    //     int max = 0;
+    //     for(int i = 0; i<nums.length; i++){
+    //          if (i > max) return false;
+    //         int maxjump = nums[i] + i;
+    //         max = Math.max(max, maxjump);
+    //         if(max>=nums.length-1){
+    //             return true;
+    //         }
+    //     }
+    //     return false;  
+    // }
+
     public boolean canJump(int[] nums) {
-        int max = 0;
-        for(int i = 0; i<nums.length; i++){
-             if (i > max) return false;
-            int maxjump = nums[i] + i;
-            max = Math.max(max, maxjump);
-            if(max>=nums.length-1){
-                return true;
+        int n=nums.length;
+        boolean dp[]=new boolean[n+1];
+        // Arrays.fill(dp,-1);
+        return solve(0, n - 1, nums,dp);
+    }
+
+    public static boolean solve(int idx, int n, int nums[],boolean dp[]) {
+        if (idx == n)
+            return true;
+        if(idx>n) return false;
+        if(dp[idx]) return false;
+        int maxJump=nums[idx];
+
+        for(int i=1;i<=maxJump;i++){
+            if(solve(idx+i,n,nums,dp)){
+                return dp[i]=true;
             }
         }
+        dp[idx]=true;
         return false;
-        
     }
 }
