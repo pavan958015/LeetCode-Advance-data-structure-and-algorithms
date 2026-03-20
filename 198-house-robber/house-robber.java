ac7c885999a54d15cc9e640231bda1aefc950903
Pavan Soni <pavansoni210129@gmail.com>
2026-03-20 10:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (30.39%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,65 +1,56 @@
 class Solution {
-    public int rob(int[] nums){
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return helper(0,nums,dp);
-    }
-    public static int helper(int idx,int nums[],int[] dp){
-        if(idx>=nums.length) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int take=nums[idx]+helper(idx+2,nums,dp);
-        int notTake=helper(idx+1,nums,dp);
-        return dp[idx]=Math.max(take,notTake);
-    }
+
+    //using Recrsion and DP
+
     // public int rob(int[] nums) {
-    //     int dp[]=new int[nums.length];
+    //     int n=nums.length;
+    //     int dp[]=new int[n+1];
     //     Arrays.fill(dp,-1);
-    //     return helper(nums.length-1,nums,dp);
+    //     return usingRecursion(0,n-1,nums,dp);
     // }
-    // public int helper(int n,int[] nums,int[] dp){
-    //     if(n<0) return 0;
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-    //     int accept=nums[n]+helper(n-2,nums,dp);
-    //     int skip=helper(n-1,nums,dp);
-    //     dp[n]=Math.max(accept,skip);
-    //     return dp[n];
+    // public static int usingRecursion(int idx,int n,int [] nums,int[] dp){
+    //     if(idx>n) return 0;
+    //     if(idx==n) return nums[idx];
+
+    //     if(dp[idx]!=-1) return dp[idx];
+
+    //     int pick=nums[idx]+usingRecursion(idx+2,n,nums,dp);
+    //     int Notpick=usingRecursion(idx+1,n,nums,dp);
+
+    //     return dp[idx]=Math.max(pick,Notpick);
     // }
 
     // public int rob(int[] nums){
     //     int n=nums.length;
-    //     int dp[]=new int[nums.length+1];
-    //     if(n==0) return 0;
-    //     if(n==1) return nums[0];
-    //     dp[0]=0;
-    //     dp[1]=nums[0];
-    //     for(int i=2;i<=n;i++){
-    //         int accept=nums[i-1]+dp[i-2];
-    //         int skip=dp[i-1];
-    //         dp[i]=Math.max(accept,skip);
+    //     int dp[]=new int[n+1];
+    //     Arrays.fill(dp,Integer.MIN_VALUE);
+    //     dp[0]=nums[0];
+    //     dp[1]=Math.max(nums[0],nums[1];
+
+    //     for(int i=2;i<n;i++){
+    //         int pick=nums[i]+dp[i-2];
+    //         int notPick=dp[i-1];
+    //         dp[i]=Math.max(pick,notPick);
     //     }
-    //     return dp[n];
+    //     return dp[n-1];
     // }
 
-
-    // Space Optimization
-    /*
     public int rob(int[] nums){
         int n=nums.length;
-        int prev=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++){
-            int accept=nums[i];
-            if(i>1)
-                accept+=prev2;
-            int skip=prev;
-            int cur=Math.max(accept,skip);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int prev2=nums[0];
+        int prev1=Math.max(nums[0],nums[1]);
 
-            prev2=prev;
-            prev=cur;
+        for(int i=2;i<n;i++){
+            int pick=nums[i]+prev2;
+            int notPick=prev1;
+
+            int cur=Math.max(pick,notPick);
+            prev2=prev1;
+            prev1=cur;
         }
-        return prev;
+        return prev1;
+
     }
-    */
 }
