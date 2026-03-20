54113e48696fc30ef0a1fd31f0f285408d166468
Pavan Soni <pavansoni210129@gmail.com>
2026-03-20 16:04:18 +0530

    Time: 1 ms (99.85%) | Memory: 47.2 MB (78.87%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -1,40 +1,31 @@
 class Solution {
-    
-    public static int minFallingPathSum(int matrix[][]) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        int ans = Integer.MAX_VALUE;
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=Integer.MAX_VALUE;
-            }
+    public int minFallingPathSum(int[][] matrix) {
+        int n=matrix.length;
+        
+        int ans=Integer.MAX_VALUE;
+        int dp[][]=new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],Integer.MAX_VALUE);
         }
-        // last row ke har column se start
-        for (int col = 0; col < m; col++) {
-            ans = Math.min(ans, helper(n - 1, col, matrix,dp));
+        for(int j=0;j<n;j++){
+            ans=Math.min(ans,solve(0,j,matrix,dp));
         }
         return ans;
     }
+    public static int solve(int i,int j,int matrix[][],int[][] dp){
+        int n=matrix.length;
 
-    public static int helper(int row, int col, int matrix[][],int dp[][]) {
-        if (col < 0 || col >= matrix[0].length)
-            return Integer.MAX_VALUE;
-        if (row == 0)
-            return matrix[0][col];
-        if(dp[row][col]!=Integer.MAX_VALUE){
-            return dp[row][col];
-        }
-        int s = helper(row - 1, col, matrix,dp);
-        int ld = helper(row - 1, col - 1, matrix,dp);
-        int rd = helper(row - 1, col + 1, matrix,dp);
+        if(j<0 || j>=n) return Integer.MAX_VALUE;
+        
+        if(i==n-1) return matrix[i][j];
 
-        int min = Math.min(s, Math.min(ld, rd));
-        if (min == Integer.MAX_VALUE) {
-            return dp[row][col]=Integer.MAX_VALUE;
-        }
-        return dp[row][col]=matrix[row][col] + min;
+        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
+
+        int downLeft=solve(i+1,j-1,matrix,dp);
+        int down=solve(i+1,j,matrix,dp);
+        int downRight=solve(i+1,j+1,matrix,dp);
+
+        return dp[i][j]=matrix[i][j]+Math.min(downLeft,Math.min(down,downRight));
+
     }
-
 }
