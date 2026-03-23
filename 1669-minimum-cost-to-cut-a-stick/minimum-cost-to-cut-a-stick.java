1481a659696ccc21dfa430a7da2cf2e6d5222f2f
Pavan Soni <pavansoni210129@gmail.com>
2026-03-23 12:47:28 +0530

    Time: 26 ms (47.79%) | Memory: 44.8 MB (22.81%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -2,29 +2,7 @@ class Solution {
     
     // TLE
 
-    // public int minCost(int n, int[] cuts) {
-    //     int m=cuts.length;
-    //     int arr[]=new int[m+2];
-    //     arr[0]=0;
-    //     arr[m+1]=n;
-    //     for(int i=0;i<m;i++){
-    //         arr[i+1]=cuts[i];
-    //     }
-    //     Arrays.sort(arr);
-    //     return solve(1,m,arr);
-    // }
-    // public static int solve(int i,int j,int[] nums){
-    //     if(i>j) return 0;
-
-    //     int min=Integer.MAX_VALUE;
-    //     for(int idx=i;idx<=j;idx++){
-    //         int cost=nums[j+1]-nums[i-1]+solve(i,idx-1,nums)+solve(idx+1,j,nums);
-    //         min=Math.min(min,cost);
-    //     }
-    //     return min;
-    // }
-
-
+    
 
     public int minCost(int n, int[] cuts) {
         int m=cuts.length;
@@ -34,24 +12,52 @@ class Solution {
         for(int i=0;i<m;i++){
             arr[i+1]=cuts[i];
         }
-        Arrays.sort(arr);
-
-        int dp[][]=new int[m+2][m+2];
-        for(int i=0;i<dp.length;i++){
+        int dp[][]=new int[arr.length][arr.length];
+        for(int i=0;i<arr.length;i++){
             Arrays.fill(dp[i],-1);
         }
+        Arrays.sort(arr);
         return solve(1,m,arr,dp);
     }
-    public static int solve(int i,int j,int[] nums,int[][]dp){
+    public static int solve(int i,int j,int[] nums,int dp[][]){
         if(i>j) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-
         int min=Integer.MAX_VALUE;
+        if(dp[i][j]!=-1) return dp[i][j];
         for(int idx=i;idx<=j;idx++){
             int cost=nums[j+1]-nums[i-1]+solve(i,idx-1,nums,dp)+solve(idx+1,j,nums,dp);
             min=Math.min(min,cost);
         }
         return dp[i][j]=min;
     }
+
+
+    // public int minCost(int n, int[] cuts) {
+    //     int m=cuts.length;
+    //     int arr[]=new int[m+2];
+    //     arr[0]=0;
+    //     arr[m+1]=n;
+    //     for(int i=0;i<m;i++){
+    //         arr[i+1]=cuts[i];
+    //     }
+    //     Arrays.sort(arr);
+
+    //     int dp[][]=new int[m+2][m+2];
+    //     for(int i=0;i<dp.length;i++){
+    //         Arrays.fill(dp[i],-1);
+    //     }
+    //     return solve(1,m,arr,dp);
+    // }
+    // public static int solve(int i,int j,int[] nums,int[][]dp){
+    //     if(i>j) return 0;
+
+    //     if(dp[i][j]!=-1) return dp[i][j];
+
+    //     int min=Integer.MAX_VALUE;
+    //     for(int idx=i;idx<=j;idx++){
+    //         int cost=nums[j+1]-nums[i-1]+solve(i,idx-1,nums,dp)+solve(idx+1,j,nums,dp);
+    //         min=Math.min(min,cost);
+    //     }
+    //     return dp[i][j]=min;
+    // }
 }
