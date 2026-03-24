8d746e8e0ef3f8e8370ecc95cd113f1711bde1b2
Pavan Soni <pavansoni210129@gmail.com>
2026-03-24 10:25:18 +0530

    Time: 96 ms (17.12%) | Memory: 66.5 MB (35.15%) - LeetSync

--- 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.java
@@ -1,35 +1,28 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
-        int m=nums1.length;
-        int n=nums2.length;
+        int n=nums1.length;
+        int m=nums2.length;
+
+        int dp[][]=new int[n+1][m+1];
+        for (int[] row : dp) Arrays.fill(row, -1);
         int ans=0;
-        int dp[][] = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                ans=Math.max(ans,solve(i,j,nums1,nums2,dp));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                ans=Math.max(ans,solve(i,j,n,m,nums1,nums2,dp));
             }
         }
         return ans;
     }
-    public static int solve(int m, int n, int[] nums1, int[] nums2, int[][] dp) {
-        // base 
-        if(m==nums1.length) return 0;
-        if(n==nums2.length) return 0;
+    public static int solve(int i,int j,int n,int m,int[] nums1,int[] nums2,int[][] dp){
+        if(i==0 || j==0) return 0;
 
-        if(dp[m][n] != -1){
-            return dp[m][n];
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(nums1[i-1]==nums2[j-1]){
+            dp[i][j]=1+solve(i-1,j-1,n,m,nums1,nums2,dp);
+        }else{
+            dp[i][j]=0;
         }
-        if(nums1[m] == nums2[n]){
-            dp[m][n] = 1 + solve(m+1, n+1, nums1, nums2, dp);
-            return dp[m][n];
-        } else {
-            dp[m][n] = 0;
-        }
-        return dp[m][n];
+        return dp[i][j];
     }
 }
