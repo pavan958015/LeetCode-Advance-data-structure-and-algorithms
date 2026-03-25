6c78b294d4037b427391fcdf431fdf69bfd1a3df
Pavan Soni <pavansoni210129@gmail.com>
2026-03-25 18:44:04 +0530

    Time: 20 ms (59.93%) | Memory: 63.4 MB (16.04%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,60 +1,46 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        Boolean dp[][]=new Boolean[n][m];
-        
-        return solve(n-1,m-1,s,p,dp);
+        int n = s.length();
+        int m = p.length();
+
+        int dp[][] = new int[n][m]; // -1 = not visited
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(n - 1, m - 1, s, p, dp);
     }
-    // public static boolean solve(int i,int j,String s,String p){
-    //     // base case 
-    //     if(i<0 && j<0) return true;
-    //     if(j<0 && i>=0) return false;
-    //     if(i<0 && j>=0){
-    //         for(int ii=0;ii<=j;ii++){
-    //             if(p.charAt(ii)!='*'){
-    //                 return false;
-    //             }
-    //         }
-    //         return true;
-    //     }
-    //     // recursion
-    //     // if match
-    //     if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-    //         return solve(i-1,j-1,s,p);
-    //     }
-    //     if(p.charAt(j)=='*'){
-    //         return solve(i-1,j,s,p) || solve(i,j-1,s,p);
-    //     }
-    //     return false;
-    // }
 
+    public boolean solve(int i, int j, String s, String p, int dp[][]) {
+        // base cases
+        if (i < 0 && j < 0) return true;
 
+        if (j < 0 && i >= 0) return false;
 
-    // using Dp
-    public static boolean solve(int i,int j,String s,String p,Boolean[][]dp){
-        // base case 
-        if(i<0 && j<0) return true;
-        if(j<0 && i>=0) return false;
-        if(i<0 && j>=0){
-            for(int ii=0;ii<=j;ii++){
-                if(p.charAt(ii)!='*'){
-                    return false;
-                }
+        if (i < 0 && j >= 0) {
+            for (int x = 0; x <= j; x++) {
+                if (p.charAt(x) != '*') return false;
             }
             return true;
         }
-        if(dp[i][j]!=null){
-            return dp[i][j];
+
+        if (dp[i][j] != -1) return dp[i][j] == 1;
+
+        // match or '?'
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
+            dp[i][j] = solve(i - 1, j - 1, s, p, dp) ? 1 : 0;
+            return dp[i][j] == 1;
         }
-        // recursion
-        // if match
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-            return dp[i][j]= solve(i-1,j-1,s,p,dp);
+
+        // '*'
+        if (p.charAt(j) == '*') {
+            boolean res = solve(i - 1, j, s, p, dp) || solve(i, j - 1, s, p, dp);
+            dp[i][j] = res ? 1 : 0;
+            return res;
         }
-        if(p.charAt(j)=='*'){
-            return dp[i][j]= solve(i-1,j,s,p,dp) || solve(i,j-1,s,p,dp);
-        }
-        return dp[i][j]=false;
+
+        dp[i][j] = 0;
+        return false;
     }
 }
