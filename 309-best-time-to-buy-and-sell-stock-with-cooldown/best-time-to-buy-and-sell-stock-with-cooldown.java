0001691c431fe2e3c4f7d8b2ecfd069aae7ada87
Pavan Soni <pavansoni210129@gmail.com>
2026-03-25 23:38:51 +0530

    Time: 1 ms (70.55%) | Memory: 43.7 MB (41.95%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -9,7 +9,8 @@ class Solution {
                     dp[i][j] = -1;
             }
         }
-        return solve(0, 1, prices,dp);
+        // return solve(0, 1, prices,dp);
+        return maxProfitTabulation(prices);
     }
 public static int solve(int idx, int flag, int arr[], int dp[][]) {
         if (idx >= arr.length)
@@ -34,24 +35,17 @@ public static int solve(int idx, int flag, int arr[], int dp[][]) {
 
     public static int maxProfitTabulation(int[] prices) {
         int n = prices.length;
-        int dp[][] = new int[n + 1][2];
+        int dp[][] = new int[n + 2][2];
 
         for (int i = n - 1; i >= 0; i--) {
             for (int j = 0; j < 2; j++) {
                 // buy
                 dp[i][1] = Math.max(-prices[i] + dp[i + 1][0], dp[i + 1][1]);
                 // cell
-                dp[i][0] = Math.max(prices[i] + dp[i + 1][1], dp[i + 1][0]);
+                dp[i][0] = Math.max(prices[i] + dp[i + 2][1], dp[i + 1][0]);
             }
         }
-        // print
-
-        for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[0].length; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
+    
         return dp[0][1];
     }
 }
