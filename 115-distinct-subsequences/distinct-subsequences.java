4776be157c299ebbe7f839f71bc015abded7a669
Pavan Soni <pavansoni210129@gmail.com>
2026-03-25 17:03:09 +0530

    Time: 19 ms (70.23%) | Memory: 54.5 MB (46.02%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,46 +2,25 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
-        int dp[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dp[i][j]=-1;
-            }
+
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
         }
-        return solve(n-1,m-1,s,t,dp);
+
+        return solve(n,m,s,t,dp);
     }
+    public static int solve(int i,int j,String s,String t,int[][] dp){
+        // base case
+        if(j<=0) return 1;
+        if(i<=0) return 0;
 
-    // using Recursion
-
-    // public static int solve(int i,int j,String s,String t){
-    //     // base case;
-    //     if(j<0) return 1;
-    //     if(i<0) return 0;
-
-    //     // recursive
-    //     if(s.charAt(i)==t.charAt(j)){
-    //         return solve(i-1,j-1,s,t)+solve(i-1,j,s,t);
-    //     }
-    //     else{
-    //         return solve(i-1,j,s,t);
-    //     }
-    // }
-
-
-    // using DP
-    public static int solve(int i,int j,String s,String t,int dp[][]){
-        // base case;
-        if(j<0) return 1;
-        if(i<0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
 
-        // recursive
-        if(s.charAt(i)==t.charAt(j)){
-            dp[i][j]= solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
+        if(s.charAt(i-1)==t.charAt(j-1)){
+            return dp[i][j]=solve(i-1,j-1,s,t,dp)+solve(i-1,j,s,t,dp);
+        }else{
+            return dp[i][j]=solve(i-1,j,s,t,dp);
         }
-        else{
-            dp[i][j]= solve(i-1,j,s,t,dp);
-        }
-        return dp[i][j];
     }
 }
