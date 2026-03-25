328adb3367a433f158817463b1ae6c2d8cf69eda
Pavan Soni <pavansoni210129@gmail.com>
2026-03-26 00:24:37 +0530

    Time: 32 ms (24.71%) | Memory: 78.4 MB (18.00%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+
+        int n = prices.length;
+        int dp[][] = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(0, n, 1,fee, prices, dp);
+        // return tabulation(prices,fee);
+    }
+
+    public static int solve(int idx, int n, int canByOrNot, int fee, int[] prices, int[][] dp) {
+        if (idx == n)
+            return 0;
+
+        if (dp[idx][canByOrNot] != -1)
+            return dp[idx][canByOrNot];
+
+        int profit = 0;
+        if (canByOrNot == 1) {
+            // buy
+            profit = Math.max(-prices[idx] + solve(idx + 1, n, 0, fee, prices, dp),
+                    solve(idx + 1, n, 1, fee, prices, dp));
+        } // sell 
+        else {
+            profit = Math.max(prices[idx] - fee + solve(idx + 1, n, 1, fee, prices, dp),
+                    solve(idx + 1, n, 0, fee, prices, dp));
+        }
+        return dp[idx][canByOrNot] = profit;
+    }
+
+    public static int tabulation(int[] prices, int fee) {
+        int n = prices.length;
+        int dp[][] = new int[n + 1][2];
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                int profit = 0;
+                if (buy == 1) {
+                    // buy
+                    profit = Math.max(-prices[idx] + dp[idx + 1][0], dp[idx + 1][1]);
+                } // sell 
+                else {
+                    profit = Math.max(prices[idx] - fee + dp[idx + 1][1], dp[idx + 1][0]);
+                }
+                dp[idx][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+}
