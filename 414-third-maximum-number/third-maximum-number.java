b5b311fce984c64026a12b1660a5f8109953fbe6
Pavan Soni <pavansoni210129@gmail.com>
2026-03-27 03:18:18 +0530

    Time: 3 ms (77.19%) | Memory: 44.8 MB (78.37%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,12 +1,41 @@
 class Solution {
+    // public int thirdMax(int[] nums) {
+    //     Set<Integer> set = new HashSet<>();
+    //     for (int x : nums)
+    //         set.add(x);
+
+    //     List<Integer> list = new ArrayList<>(set);
+    //     Collections.sort(list);
+
+    //     if (list.size() >= 3)
+    //         return list.get(list.size() - 3);
+    //     else
+    //         return list.get(list.size() - 1);
+    // }
+
     public int thirdMax(int[] nums) {
-        Set<Integer> set=new HashSet<>();
-        for(int x:nums) set.add(x);
+        Integer  first = null;
+        Integer  second = null;
+        Integer  third = null;
 
-        List<Integer> list=new ArrayList<>(set);
-        Collections.sort(list);
+        for (int i = 0; i < nums.length; i++) {
+            if ((first != null && nums[i] == first) ||
+                    (second != null && nums[i] == second) ||
+                    (third != null && nums[i] == third))
+                continue;
+            if (first==null || nums[i] > first) {
+                third = second;
+                second = first;
+                first = nums[i];
 
-        if(list.size()>=3) return list.get(list.size()-3);
-        else return list.get(list.size()-1);
+            } else if (second==null  || nums[i] > second) {
+                third = second;
+                second = nums[i];
+
+            } else if(third==null || nums[i]>third) {
+                third = nums[i];
+            }
+        }
+        return third==null?first:third;
     }
 }
