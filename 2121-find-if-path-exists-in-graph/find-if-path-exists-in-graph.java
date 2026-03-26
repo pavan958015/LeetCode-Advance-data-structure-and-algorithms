ee3c08c232499ff2afae420b566c1ea17d057a5a
Pavan Soni <pavansoni210129@gmail.com>
2026-03-27 05:14:41 +0530

    Time: 125 ms (55.47%) | Memory: 276.5 MB (76.54%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,44 +1,31 @@
 class Solution {
+    public static boolean dfs(int source,int destination,List<List<Integer>> adj,boolean[] vis){
+        vis[source]=true;
+        if(source==destination) return true;
+
+        for(int x:adj.get(source)){
+            if(!vis[x]){
+                if(x==destination) return true;
+                if(dfs(x,destination,adj,vis))
+                    return true;
+            }
+        }
+        return false;
+    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
         for(int i=0;i<edges.length;i++){
-            
-            adj.get(edges[i][0]).add(edges[i][1]);
-            adj.get(edges[i][1]).add(edges[i][0]);
-        } 
-        boolean vis[]=new boolean[n];
-        return usingDFS(adj,source,destination,vis);
-    }
-    // public static boolean usingBFS(List<List<Integer>> adj, int source, int destination,boolean[] vis){
-    //     if(source==destination) return true;
+            int u=edges[i][0];
+            int v=edges[i][1];
 
-    //     Queue<Integer> q=new LinkedList<>();
-    //     vis[source]=true;
-    //     q.add(source);
-    //     while(!q.isEmpty()){
-    //         int node =q.poll();
-    //         for(int x:adj.get(source)){
-    //             if(x==destination) return true;
-    //             if(!vis[x]){
-    //                 q.add(x);
-    //                 vis[x]=true;
-    //             }
-    //         }
-    //     }
-    //     return false;
-    // }
-    public static boolean usingDFS(List<List<Integer>> adj,int source,int destination,boolean[] vis){
-        if(source==destination) return true;
-        vis[source]=true;
-        for(int node:adj.get(source)){
-            if(node==destination) return true;
-            if(!vis[node]){
-                if(usingDFS(adj,node,destination,vis)) return true;
-            }
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
         }
-        return false;
+        boolean vis[]=new boolean[n];
+        return dfs(source,destination,adj,vis);
     }
 }
