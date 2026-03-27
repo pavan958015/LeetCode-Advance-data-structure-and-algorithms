b280afea06624d166ab62dededdb5ddd6bd232a9
Pavan Soni <pavansoni210129@gmail.com>
2026-03-27 13:19:41 +0530

    Time: 2 ms (65.29%) | Memory: 47.2 MB (6.96%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,80 +1,36 @@
 class Solution {
-    // public static int dfs(int i,int j,int[][] grid,boolean vis[][],int n,int m){
-    //     if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0 || vis[i][j]){
-    //         return 0;
-    //     }
-    //         vis[i][j]=true;
+    public static int dfs(int i, int j, int[][] grid, boolean[][] vis) {
+        if (i < 0 || j < 0 || i>=grid.length || j>=grid[0].length || grid[i][j] == 0 || vis[i][j])
+            return 0;
 
-    //     int left=dfs(i,j-1,grid,vis,n,m);
-    //     int right=dfs(i,j+1,grid,vis,n,m);
-    //     int up=dfs(i-1,j,grid,vis,n,m);
-    //     int down=dfs(i+1,j,grid,vis,n,m);
-
-    //     return 1+left+right+up+down;
-    // }
-
-    // using bfs
-    public static int bfs(int i, int j, int[][] grid, boolean vis[][], int n, int m) {
-        Queue<int[]> q = new LinkedList<>();
         vis[i][j] = true;
-        q.add(new int[] { i, j });
 
-        int area = 1;
+        int dx[] = { -1, 1, 0, 0 };
+        int dy[] = { 0, 0, -1, 1 };
 
-        while (!q.isEmpty()) {
-            int cur[] = q.poll();
-            int r = cur[0];
-            int c = cur[1];
+        int left = dfs(i, j - 1, grid, vis);
+        int up = dfs(i - 1, j, grid, vis);
+        int right = dfs(i, j + 1, grid, vis);
+        int down = dfs(i + 1, j, grid, vis);
 
-            // up
-
-            if (r - 1 >= 0 && grid[r - 1][c] == 1 && !vis[r - 1][c]) {
-                vis[r - 1][c] = true;
-                q.add(new int[] { r - 1, c });
-                area++;
-            }
-            //down
-
-            if (r + 1 < n && grid[r + 1][c] == 1 && !vis[r + 1][c]) {
-                vis[r + 1][c] = true;
-                q.add(new int[] { r + 1, c });
-                area++;
-            }
-            // left
-
-            if (c - 1 >= 0 && grid[r][c - 1] == 1 && !vis[r][c - 1]) {
-                vis[r][c - 1] = true;
-                q.add(new int[] { r, c - 1 });
-                area++;
-            }
-
-            // right
-            if (c + 1 < m && grid[r][c + 1] == 1 && !vis[r][c + 1]) {
-                vis[r][c + 1] = true;
-                q.add(new int[] { r, c + 1 });
-                area++;
-            }
-        }
-        return area;
+        return 1+left+right+up+down;
     }
 
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-
-        int ans = 0;
+        int maxArea = 0;
         boolean vis[][] = new boolean[n][m];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!vis[i][j] && grid[i][j] == 1) {
-                    // int res=dfs(i,j,grid,vis,n,m);
-                    int res = bfs(i, j, grid, vis, n, m);
-                    ans = Math.max(ans, res);
+                if (grid[i][j] == 1) {
+                    if (!vis[i][j]) {
+                        maxArea = Math.max(maxArea, dfs(i, j, grid, vis));
+                    }
                 }
             }
         }
-        return ans;
+        return maxArea;
     }
-
 }
