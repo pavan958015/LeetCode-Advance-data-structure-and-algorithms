5cc4d3f11bf567a87793b7aeb3daad08cf062666
Pavan Soni <pavansoni210129@gmail.com>
2026-03-28 22:26:47 +0530

    Time: 2 ms (100.00%) | Memory: 46.4 MB (85.13%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -1,40 +1,33 @@
 class Solution {
-    static int n;
-    static int m;
-    static boolean vis[][];
+    public static boolean dfs(int i,int j,int[][] grid,boolean[][] vis){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length) return false;
 
-    public static boolean dfs(int i, int j, int[][] grid) {
-        if (i < 0 || j < 0 || i >= n || j >= m)
-            return false;
+        if (grid[i][j] == 1 || vis[i][j]) {
+        return true; 
+        }
 
-        if (vis[i][j])
-            return true;
-        if (grid[i][j] == 1)
-            return true;
+        vis[i][j]=true;
 
-        vis[i][j] = true;
+        boolean down=dfs(i+1,j,grid,vis);
+        boolean up=dfs(i-1,j,grid,vis);
+        boolean right=dfs(i,j+1,grid,vis);
+        boolean left=dfs(i,j-1,grid,vis);
 
-        boolean left = dfs(i, j - 1, grid);
-        boolean right = dfs(i, j + 1, grid);
-        boolean up = dfs(i - 1, j, grid);
-        boolean down = dfs(i + 1, j, grid);
-
-        return left && right && up && down;
+        return down && up && right && left;
     }
-
     public int closedIsland(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        int count = 0;
-        vis = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!vis[i][j]) {
-                    if (grid[i][j] == 0) {
-                        if (dfs(i, j, grid)) {
-                            count++;
-                        }
-                    }
+        int n=grid.length;
+        int m=grid[0].length;
+
+        boolean vis[][]=new boolean[n][m];
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0)
+                    if(!vis[i][j])
+                if(dfs(i,j,grid,vis)){
+                    count++;
                 }
             }
         }
