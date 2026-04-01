6905e211d6887351233246e240c565da595ed522
Pavan Soni <pavansoni210129@gmail.com>
2026-04-01 14:57:02 +0530

    Time: 3 ms (69.50%) | Memory: 45 MB (90.06%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,14 +1,30 @@
 class Solution {
-    public int singleNumber(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(int key:map.keySet()){
-            if(map.get(key)==1){
-                return key;
+    // public int singleNumber(int[] nums) {
+    //     Map<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+    //     }
+    //     for(int key:map.keySet()){
+    //         if(map.get(key)==1){
+    //             return key;
+    //         }
+    //     }
+    //     return -1;
+    // }
+
+
+    public int singleNumber(int nums[]){
+        int n=nums.length;
+        int ans=0;
+        for(int bitIndex=0;bitIndex<=31;bitIndex++){
+            int count=0;
+            for(int i=0;i<n;i++){
+                if((nums[i] & (1<<bitIndex))!=0){
+                    count++;
+                }
             }
+            if(count%3==1) ans=ans| (1<<bitIndex);
         }
-        return -1;
+        return ans;
     }
 }
