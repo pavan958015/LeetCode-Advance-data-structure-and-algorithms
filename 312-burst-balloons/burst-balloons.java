dcf5ebc87522e934539a860ee8234cb6a90633ad
Pavan Soni <pavansoni210129@gmail.com>
2026-04-16 22:01:35 +0530

    Time: 119 ms (19.47%) | Memory: 46.7 MB (8.31%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,35 +1,31 @@
 class Solution {
     public int maxCoins(int[] nums) {
-        int n = nums.length;
-        int[] arr = new int[n + 2];
+        // add val in front and last 1
+        int arr[]=new int[nums.length+2];
+        int n=nums.length;
 
-        arr[0] = 1;
-        arr[n + 1] = 1;
+        arr[0]=1;
+        arr[nums.length+1]=1;
 
-        for (int i = 0; i < n; i++) {
-            arr[i + 1] = nums[i];
+        for(int i=0;i<nums.length;i++){
+            arr[i+1]=nums[i];
         }
+        Integer dp[][]=new Integer[n+2][n+2];
 
-        int dp[][] = new int[n + 2][n + 2];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-
-        return solve(1, n, arr, dp);
+        return solve(1,n,arr,dp);
     }
+    public static int solve(int i,int j,int[] arr,Integer [][]dp){
+        if(i>j) return 0;
 
-    public static int solve(int i, int j, int[] arr, int[][] dp) {
-        if (i > j)
-            return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        int max=Integer.MIN_VALUE;
+        for(int idx=i;idx<=j;idx++){
+            int cost=arr[i-1]*arr[idx]*arr[j+1]+solve(i,idx-1,arr,dp)+solve(idx+1,j,arr,dp);
+            max=Math.max(cost,max);
 
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int max = Integer.MIN_VALUE;
-        for (int idx = i; idx <= j; idx++) {
-            int cost = arr[i - 1] * arr[idx] * arr[j + 1] + solve(i, idx - 1, arr, dp) + solve(idx + 1, j, arr, dp);
-            max = Math.max(max, cost);
         }
-        return dp[i][j] = max;
+        dp[i][j]=max;
+        return dp[i][j];
     }
+    
 }
