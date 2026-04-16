c1fb6abc073039de157d6f581d9921bcc5421745
Pavan Soni <pavansoni210129@gmail.com>
2026-04-16 08:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (84.18%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,62 +1,57 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-        int total=0;
-        int[] prefixSum=new int[n+1];
-        int[] suffixSum=new int[n+1];
+        int leftMax=0,rightMax=0,total=0;
+        int left=0; int right=n-1;
 
-        prefixSum[0]=height[0];
-        for(int i=1;i<n;i++){
-            prefixSum[i]=Math.max(prefixSum[i-1],height[i]);
-        }
-
-
-        suffixSum[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffixSum[i]=Math.max(suffixSum[i+1],height[i]);
-        }
-
-        for(int i=0;i<n;i++){
-            int leftMax=prefixSum[i]; int rightMax=suffixSum[i];
-            if(height[i]<leftMax && height[i]<rightMax){
-                total+=Math.min(leftMax,rightMax)-height[i];
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>leftMax){
+                    leftMax=height[left];
+                }else{
+                    total+=leftMax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>rightMax){
+                    rightMax=height[right];
+                }else{
+                    total+=rightMax-height[right];
+                }
+                right--;
             }
         }
         return total;
+
     }
 }
+// extra Space
+
 
 // class Solution {
-//     public int[] getLeftMax(int[] height, int n) {
-//         int[] leftMax = new int[n];
-//         leftMax[0] = height[0];
-//         for (int i = 1; i < n; i++) {
-//             leftMax[i] = Math.max(leftMax[i - 1], height[i]);
-//         }
-//         return leftMax;
-//     }
-
-//     public int[] getRightMax(int[] height, int n) {
-//         int[] rightMax = new int[n];
-//         rightMax[n - 1] = height[n - 1];
-//         for (int i = n - 2; i >= 0; i--) {
-//             rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-//         }
-//         return rightMax;
-//     }
-
 //     public int trap(int[] height) {
 //         int n = height.length;
-//         if (n == 1 || n == 0)
-//             return 0;
-        
-//         int[] leftMax = getLeftMax(height, n);
-//         int[] rightMax = getRightMax(height, n);
+//         int total = 0;
+//         int[] prefixSum = new int[n + 1];
+//         int[] suffixSum = new int[n + 1];
 
-//         int sum = 0;
-//         for (int i = 0; i < n; i++) {
-//             sum += Math.min(leftMax[i], rightMax[i]) - height[i];
+//         prefixSum[0] = height[0];
+//         for (int i = 1; i < n; i++) {
+//             prefixSum[i] = Math.max(prefixSum[i - 1], height[i]);
 //         }
-//         return sum;
+
+//         suffixSum[n - 1] = height[n - 1];
+//         for (int i = n - 2; i >= 0; i--) {
+//             suffixSum[i] = Math.max(suffixSum[i + 1], height[i]);
+//         }
+
+//         for (int i = 0; i < n; i++) {
+//             int leftMax = prefixSum[i];
+//             int rightMax = suffixSum[i];
+//             if (height[i] < leftMax && height[i] < rightMax) {
+//                 total += Math.min(leftMax, rightMax) - height[i];
+//             }
+//         }
+//         return total;
 //     }
 // }
