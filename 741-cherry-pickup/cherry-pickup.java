15d084b1cd5540e6e6e75c4c63f73ca95f977d13
Pavan Soni <pavansoni210129@gmail.com>
2026-04-17 14:14:27 +0530

    Time: 98 ms (18.93%) | Memory: 278.3 MB (25.66%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.java
+++ 741-cherry-pickup/cherry-pickup.java
@@ -1,61 +1,36 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        // ( 0 , 0 ) to ( n - 1 ) ( n - 1 )
-        //then
-        // ( n - 1 ) ( n - 1 ) to ( 0 , 0 );
+        int n=grid.length;
+        Integer dp[][][][]=new Integer[n+1][n+1][n+1][n+1];
 
-        // will lose
-
-
-        // OR
-
-        // traverse both 
-
-        // (0,0)  to (n-1)(n-1)
-
-        int n = grid.length;
-        int dp[][][][]=new int[n][n][n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    Arrays.fill(dp[i][j][k], -1);
-                }
-            }
-        }
-
-        int ans = dfs(0, 0, 0, 0, n, grid,dp);
-        return Math.max(0, ans);
+        int ans=solve(0,0,0,0,n-1,grid,dp);
+        return Math.max(0,ans);
     }
+    public static int solve(int r1,int c1,int r2,int c2,int n,int[][]grid,Integer[][][][]dp){
+        if(r1>n || r2>n || c1>n || c2>n || grid[r1][c1]==-1 || grid[r2][c2]==-1) return Integer.MIN_VALUE;
 
-    public static int dfs(int r1, int r2, int c1, int c2, int n, int[][] grid,int[][][][]dp) {
-        if (r1 >= n || r2 >= n || c1 >= n || c2 >= n || grid[r1][c1] == -1 || grid[r2][c2] == -1)
-            return Integer.MIN_VALUE;
+        if(r1==n && c1==n || r2==n && c2==n) return grid[r1][c1];;
 
-        // if (r1 + c1 != r2 + c2)
-        //     return Integer.MIN_VALUE;
+        if(dp[r1][c1][r2][c2]!=null) return dp[r1][c1][r2][c2];
 
-        if (r1 == n - 1 && c1 == n - 1 && r2 == n - 1 && c2 == n - 1)
-            return grid[r1][c1];
-
-        if(dp[r1][c1][r2][c2]!=-1) return dp[r1][c1][r2][c2];
-
-        int charry = 0;
-        // take once if r1 c1 and r2 and c2 in same cell
-        if (r1 == r2 && c1 == c2) {
-            charry += grid[r1][c1];
-        } else {
-            charry += grid[r1][c1] + grid[r2][c2];
+        int charry=0;
+        if(r1==r2 && c1==c2){
+            charry=grid[r1][c1];
+        }else{
+            charry=grid[r1][c1]+grid[r2][c2];
         }
 
-        int a = dfs(r1 + 1, r2 + 1, c1, c2, n, grid,dp);
-        int b = dfs(r1 + 1, r2, c1, c2 + 1, n, grid,dp);
-        int c = dfs(r1, r2 + 1, c1 + 1, c2, n, grid,dp);
-        int d = dfs(r1, r2, c1 + 1, c2 + 1, n, grid,dp);
+    // Both Down
+    int a = solve(r1 + 1, c1, r2 + 1, c2, n, grid, dp);
+    //  Down Right
+    int b = solve(r1 + 1, c1, r2, c2 + 1, n, grid, dp);
+    // Right Down
+    int c = solve(r1, c1 + 1, r2 + 1, c2, n, grid, dp);
+    // Both Right
+    int d = solve(r1, c1 + 1, r2, c2 + 1, n, grid, dp);
+        
+        return dp[r1][c1][r2][c2]=charry+Math.max(Math.max(a,b),Math.max(c,d));
 
-
-        int total = Math.max(Math.max(a, b), Math.max(c, d));
-        charry += total;
-
-        return dp[r1][c1][r2][c2]=charry;
+        
     }
 }
