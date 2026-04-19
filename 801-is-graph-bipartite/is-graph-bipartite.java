e7cf6f413540e66c1cc526977a029e4a3b6e18a2
Pavan Soni <pavansoni210129@gmail.com>
2026-04-19 18:59:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (23.41%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,32 +1,45 @@
 class Solution {
-    public static boolean check(int source,int[]color,int[][] graph){
-        color[source]=1;
-        Queue<Integer> q=new LinkedList<>();
-        q.add(source);
+    // public static boolean check(int start,int v,int graph[][],int color[]){
+    //     color[start]=0;
 
-        while(!q.isEmpty()){
-            int cur=q.poll();
-            for(int x:graph[cur]){
-                if(color[x]==-1){
-                    color[x]=1-color[cur];
-                    q.add(x);
+    //     Queue<Integer> q=new LinkedList<>();
+    //     q.add(start);
+    //     while(!q.isEmpty()){
+    //         int cur=q.poll();
+
+    //         for(int x:graph[cur]){
+    //             if(color[x]==-1){
+    //                 q.add(x);
+    //                 color[x]=1-color[cur];
+    //             }else if(color[x]==color[cur]) return false;
+    //         }
+    //     }
+    //     return true;
+
+    // }
+
+    public static boolean DFS(int start,int curColor,int graph[][],int color[]){
+        color[start]=curColor;
+
+        for(int x:graph[start]){
+            if(color[x]==-1){
+                if(!DFS(x,1-curColor,graph,color)){
+                     return false;
                 }
-                else if(color[x]==color[cur]){
-                    return false;
-                }
-            }
+
+            }else if(color[x]==curColor) return false;
         }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
-        int V=graph.length;
-        int[] color=new int[V];
+        int v=graph.length;
+        int color[]=new int[v];
         Arrays.fill(color,-1);
-        
-        for(int i=0;i<V;i++){
-            if(color[i]==-1){
-                if(!check(i,color,graph)) return false;
-            }
+        for(int i=0;i<v;i++){
+            if(color[i]==-1)
+                // if(!check(i,v,graph,color)) return false;
+                if(!DFS(i,0,graph,color)) return false;
+
         }
         return true;
     }
