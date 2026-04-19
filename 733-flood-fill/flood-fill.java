8160b4102f835bca13158002eae5caf628fd581e
Pavan Soni <pavansoni210129@gmail.com>
2026-04-19 22:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (81.01%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,33 +1,25 @@
 class Solution {
+    public static void dfs(int i,int j,int old,int color,int[][] image){
+        int n=image.length; int m=image[0].length;
+        if(i<0 || i>=n || j<0 || j>=m || image[i][j]!=old) return;
 
-    // ** vis is not required because don't need vis after change colr of the image 
+        image[i][j]=color;
 
-    public void dfs(int i, int j, int[][] image, int oldColor, int newColor, int n, int m) {
+        dfs(i+1,j,old,color,image);
+        dfs(i,j+1,old,color,image);
+        dfs(i-1,j,old,color,image);
+        dfs(i,j-1,old,color,image);
 
-        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=oldColor)
-            return;
-
-        image[i][j] = newColor;
-
-        dfs(i-1, j, image, oldColor, newColor, n, m);
-        dfs(i+1, j, image, oldColor, newColor, n, m);
-        dfs(i, j-1, image, oldColor, newColor, n, m);
-        dfs(i, j+1, image, oldColor, newColor, n, m);
     }
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n=image.length;
+        int m=image[0].length;
 
-        int n = image.length;
-        int m = image[0].length;
+        int oldColor=image[sr][sc];
 
-        int oldColor = image[sr][sc];
+        if(oldColor==color) return image;
 
-        // ** if color is same 
-        if(oldColor == color)
-            return image;
-
-        dfs(sr, sc, image, oldColor, color, n, m);
-
+        dfs(sr,sc,oldColor,color,image);
         return image;
     }
 }
