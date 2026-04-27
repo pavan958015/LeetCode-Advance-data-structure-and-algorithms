f612482d44e1b5ed7b9dd5a164be9e1f5dffcbdf
Pavan Soni <pavansoni210129@gmail.com>
2026-04-27 14:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (65.50%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        // we know that we can rotate one 
-        // so if more then one conditions are true then it will false;
-
-        int n=nums.length;
-        int count=0;
-
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%n]) count++;
-        }
-        return count<=1;
+        int n = nums.length;
+        int c = 0;
+      
+    for(int i = 0; i<n; i++){
+       if(nums[i]>nums[(i+1)%n]){
+        c++;
+       }
+    }
+    if(c>1){
+        return false;
+    } return true;
     }
 }
