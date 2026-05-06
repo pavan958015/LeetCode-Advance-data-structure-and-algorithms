f31d009aaa213ba52b05b311ff6020d932003277
Pavan Soni <pavansoni210129@gmail.com>
2026-05-06 13:56:27 +0530

    Time: 7 ms (93.03%) | Memory: 124.9 MB (28.12%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -14,8 +14,6 @@ class Solution {
                     emptySlot--;
                 }else if(boxGrid[i][j]=='*'){
                     emptySlot=j-1;
-                }else if(boxGrid[i][j]=='.'){
-
                 }
             }   
         }
@@ -27,3 +25,41 @@ class Solution {
         return finalGrid;
     }
 }
+
+
+
+// class Solution {
+//     public char[][] rotateTheBox(char[][] box) {
+
+//         int n = box.length;
+//         int m = box[0].length;
+
+//         char[][] res = new char[m][n];
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = 0; j < m; j++) {
+//                 res[j][n - 1 - i] = box[i][j];
+//             }
+//         }
+
+//         for(int col = 0; col < n; col++) {
+
+//             int empty = m - 1;
+
+//             for(int row = m - 1; row >= 0; row--) {
+
+//                 if(res[row][col] == '*') {
+//                     empty = row - 1;
+//                 }
+//                 else if(res[row][col] == '#') {
+
+//                     res[row][col] = '.';
+//                     res[empty][col] = '#';
+//                     empty--;
+//                 }
+//             }
+//         }
+
+//         return res;
+//     }
+// }
