7c2550c2c800323f31f0bfad8aa20de9f7602516
Pavan Soni <pavansoni210129@gmail.com>
2026-05-13 10:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (35.24%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,28 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public static ListNode reverse(ListNode head) {
-        ListNode cur = head;
-        ListNode prev = null;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1)
+            return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        ListNode cur = dummy.next;
 
         while (cur != null) {
-            ListNode next = cur.next;
-            cur.next = prev;
+            ListNode kth = findKth(cur, k);
+
+            if (kth == null) {
+                prev.next = cur;
+                break;
+            }
+
+            ListNode next = kth.next;
+            kth.next = null;
+
+            prev.next = reverse(cur);
             prev = cur;
             cur = next;
         }
-        return prev;
+        return dummy.next;
     }
 
-    public static ListNode findNode(ListNode temp, int k) {
+    public static ListNode findKth(ListNode temp, int k) {
         k -= 1;
         while (temp != null && k > 0) {
             temp = temp.next;
@@ -31,33 +35,15 @@ class Solution {
         return temp;
     }
 
-    public ListNode reverseKGroup(ListNode head, int k) {
-        ListNode temp = head;
-        ListNode nextNode = null;
-        ListNode prevNode = null;
-
-        while (temp != null) {
-            ListNode kthNode = findNode(temp, k);
-
-            if (kthNode == null) {
-                if (prevNode != null)
-                    prevNode.next = temp;
-                break;
-            } else {
-                nextNode = kthNode.next;
-                kthNode.next = null;
-
-                ListNode newNode = reverse(temp);
-
-                if (temp == head) {
-                    head = kthNode;
-                } else {
-                    prevNode.next = kthNode;
-                }
-                prevNode = temp;
-                temp = nextNode;
-            }
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
-        return head;
+        return prev;
     }
 }
