1e9f1b3f1f0124a18404562105a8a34e282c5c5e
Pavan Soni <pavansoni210129@gmail.com>
2026-05-17 17:48:44 +0530

    Time: 8 ms (45.03%) | Memory: 48.7 MB (73.29%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,24 +1,52 @@
 class Solution {
-    public boolean canReach(int[] arr, int start) {
 
-        // out of bounds
-        if(start < 0 || start >= arr.length)
-            return false;
+    // using BFS
+    public boolean canReach(int[] arr,int start){
+        Queue<Integer> q=new LinkedList<>();
+        boolean vis[]=new boolean[arr.length];
 
-        // already visited
-        if(arr[start] < 0)
-            return false;
+        q.add(start);
+        while(!q.isEmpty()){
+            int cur=q.poll();
 
-        // reached zero
-        if(arr[start] == 0)
-            return true;
+            if(arr[cur]==0) return true;
 
-        int jump = arr[start];
+            int next=cur+arr[cur];
+            int prev=cur-arr[cur];
 
-        // mark visited
-        arr[start] = -arr[start];
-
-        return canReach(arr, start + jump) ||
-               canReach(arr, start - jump);
+            if(next>=0 && next<arr.length && !vis[next]){
+                q.add(next);
+                vis[next]=true;
+            }
+            if(prev>=0 && prev<arr.length && !vis[prev]){
+                q.add(prev);
+                vis[prev]=true;
+            }
+        }
+        return false;
     }
+    // using Recursion
+
+    // public boolean canReach(int[] arr, int start) {
+
+    //     // out of bounds
+    //     if(start < 0 || start >= arr.length)
+    //         return false;
+
+    //     // already visited
+    //     if(arr[start] < 0)
+    //         return false;
+
+    //     // reached zero
+    //     if(arr[start] == 0)
+    //         return true;
+
+    //     int jump = arr[start];
+
+    //     // mark visited
+    //     arr[start] = -arr[start];
+
+    //     return canReach(arr, start + jump) ||
+    //            canReach(arr, start - jump);
+    // }
 }
