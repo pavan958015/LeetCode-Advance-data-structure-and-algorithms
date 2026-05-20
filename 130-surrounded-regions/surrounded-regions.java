eda5e08b397e996e1a1be0dc9099aaf51fdb7177
Pavan Soni <pavansoni210129@gmail.com>
2026-05-20 12:11:54 +0530

    Time: 2 ms (75.83%) | Memory: 47.5 MB (84.93%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,39 +1,35 @@
 class Solution {
+    public static void dfs(int i,int j,int n,int m,char[][] board,boolean vis[][]){
+        if(i<0 || i>=n || j<0 || j>=m || board[i][j]!='O' || vis[i][j]) return;
 
-    public static void dfs(int i, int j, char[][] board, boolean[][] vis, int n, int m){
-        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] || board[i][j] != 'O')
-            return;
+        vis[i][j]=true;
+        board[i][j]='T';
 
-        vis[i][j] = true;
-        board[i][j] = 'T';
+        dfs(i+1,j,n,m,board,vis);
+        dfs(i-1,j,n,m,board,vis);
+        dfs(i,j+1,n,m,board,vis);
+        dfs(i,j-1,n,m,board,vis);
 
-        dfs(i+1,j,board,vis,n,m);
-        dfs(i-1,j,board,vis,n,m);
-        dfs(i,j+1,board,vis,n,m);
-        dfs(i,j-1,board,vis,n,m);
     }
-
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        int n=board.length;
+        int m=board[0].length;
 
-        boolean[][] vis = new boolean[n][m];
-
-        // boundary
+        boolean vis[][]=new boolean[n][m];
+        // for boundary
         for(int i=0;i<n;i++){
-            if(board[i][0]=='O') dfs(i,0,board,vis,n,m);
-            if(board[i][m-1]=='O') dfs(i,m-1,board,vis,n,m);
+            if(board[i][0]=='O') dfs(i,0,n,m,board,vis);
+            if(board[i][m-1]=='O') dfs(i,m-1,n,m,board,vis);
         }
 
         for(int j=0;j<m;j++){
-            if(board[0][j]=='O') dfs(0,j,board,vis,n,m);
-            if(board[n-1][j]=='O') dfs(n-1,j,board,vis,n,m);
+            if(board[0][j]=='O') dfs(0,j,n,m,board,vis);
+            if(board[n-1][j]=='O') dfs(n-1,j,n,m,board,vis);
         }
 
-        // replace in X
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-
                 if(board[i][j]=='O') board[i][j]='X';
                 if(board[i][j]=='T') board[i][j]='O';
             }
