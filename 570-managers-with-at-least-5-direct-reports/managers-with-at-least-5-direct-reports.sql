1594a727879553d57fdee78a8a8f0a00960d57f5
Pavan Soni <pavansoni210129@gmail.com>
2026-05-24 01:44:20 +0530

    Time: 366 ms (34.99%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -3,7 +3,9 @@
 select e1.name from Employee e1
 inner join Employee e2
 where e1.id=e2.managerId
-group by e2.managerId
+-- group by e2.managerId
+GROUP BY e1.id, e1.name
+
 
 having count(e2.managerId)>=5
 
