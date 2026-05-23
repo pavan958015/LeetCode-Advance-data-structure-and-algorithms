1c604cf9e7e88ec169f395f943a13cdbf0e062ca
Pavan Soni <pavansoni210129@gmail.com>
2026-05-24 01:43:20 +0530

    Time: 358 ms (44.45%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,23 +1,9 @@
 # Write your MySQL query statement below
--- select e1.name
--- from Employee e1 inner join Employee e2
--- on e1.id=e2.managerId
--- group by e2.managerId
 
--- having count(e2.managerId)>=5
+select e1.name from Employee e1
+inner join Employee e2
+where e1.id=e2.managerId
+group by e2.managerId
 
-with recursive emp_cte as (
-    select
-        id,name,managerId from Employee
-    union all
-    select
-        e.id, e.name,e.managerId from Employee e
-    join emp_cte c
-      on e.managerId = c.id
-)
+having count(e2.managerId)>=5
 
-select m.name from Employee e 
-join Employee m 
-on e.managerId = m.id
-group by m.id, m.name
-having count(*) >= 5;
