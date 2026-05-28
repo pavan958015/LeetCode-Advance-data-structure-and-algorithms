7d6bb0eccc8891e01e305b5457bfc3ba344cf85c
Pavan Soni <pavansoni210129@gmail.com>
2026-05-28 12:20:51 +0530

    Time: 11 ms (25.22%) | Memory: 50.9 MB (37.01%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.java
@@ -1,23 +1,61 @@
+// class Solution {
+//     public TreeNode reverseOddLevels(TreeNode root) {
+//         if (root != null) {
+//             dfs(root.left, root.right, 1);
+//         }
+//         return root;
+//     }
+
+//     private void dfs(TreeNode leftNode, TreeNode rightNode, int level) {
+//         if (leftNode == null || rightNode == null) {
+//             return;
+//         }
+
+//         if (level % 2 != 0) {
+//             int temp = leftNode.val;
+//             leftNode.val = rightNode.val;
+//             rightNode.val = temp;
+//         }
+
+//         dfs(leftNode.left, rightNode.right, level + 1);
+//         dfs(leftNode.right, rightNode.left, level + 1);
+//     }
+// }
+
+
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        if (root != null) {
-            dfs(root.left, root.right, 1);
+        if (root == null) return null;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<TreeNode> currentLevelNodes = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                currentLevelNodes.add(node);
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            if (level % 2 != 0) {
+                int left = 0;
+                int right = currentLevelNodes.size() - 1;
+                while (left < right) {
+                    int temp = currentLevelNodes.get(left).val;
+                    currentLevelNodes.get(left).val = currentLevelNodes.get(right).val;
+                    currentLevelNodes.get(right).val = temp;
+                    left++;
+                    right--;
+                }
+            }
+            level++;
         }
         return root;
-    }
-
-    private void dfs(TreeNode leftNode, TreeNode rightNode, int level) {
-        if (leftNode == null || rightNode == null) {
-            return;
-        }
-
-        if (level % 2 != 0) {
-            int temp = leftNode.val;
-            leftNode.val = rightNode.val;
-            rightNode.val = temp;
-        }
-
-        dfs(leftNode.left, rightNode.right, level + 1);
-        dfs(leftNode.right, rightNode.left, level + 1);
     }
 }
