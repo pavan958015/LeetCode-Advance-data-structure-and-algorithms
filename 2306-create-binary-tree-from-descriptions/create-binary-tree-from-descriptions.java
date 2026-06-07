f96c8c0e93fbf3df9af3b86fdef5a13da4e6de6b
Pavan Soni <pavansoni210129@gmail.com>
2026-06-07 10:08:24 +0530

    Time: 77 ms (17.97%) | Memory: 86.9 MB (31.08%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -0,0 +1,45 @@
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+
+        // set for childrent node and we can find root with the help of this
+        Set<Integer> children = new HashSet<>();
+        Map<Integer, TreeNode> map = new HashMap<>();
+
+        for (int[] node : descriptions) {
+
+            int parent = node[0];
+            int child = node[1];
+            int isLeft = node[2];
+
+            children.add(child);
+
+            if (!map.containsKey(parent)) {
+                map.put(parent, new TreeNode(parent));
+            }
+
+            if (!map.containsKey(child)) {
+                map.put(child, new TreeNode(child));
+            }
+
+            TreeNode parentNode = map.get(parent);
+            TreeNode childNode = map.get(child);
+
+            if (isLeft == 1) {
+                parentNode.left = childNode;
+            } else {
+                parentNode.right = childNode;
+            }
+        }
+
+        // Root find karne ke liye
+        for (int[] node : descriptions) {
+            int parent = node[0];
+
+            if (!children.contains(parent)) {
+                return map.get(parent);
+            }
+        }
+
+        return null;
+    }
+}
