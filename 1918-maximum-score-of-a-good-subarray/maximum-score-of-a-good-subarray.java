4649ee1691b105d0955cb60fcd8fadbf4c4300f0
Pavan Soni <pavansoni210129@gmail.com>
2026-06-09 23:55:13 +0530

    Time: 8 ms (83.12%) | Memory: 83.1 MB (86.19%) - LeetSync

---
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maximumScore(int[] nums, int k) {
+        int l = k, r = k;
+        int res = nums[k];
+        int curMin = nums[k];
+
+        while (l > 0 || r < nums.length - 1) {
+
+            int left = (l > 0) ? nums[l - 1] : 0;
+            int right = (r < nums.length - 1) ? nums[r + 1] : 0;
+
+            if (left > right) {
+                l--;
+                curMin = Math.min(curMin, left);
+            } else {
+                r++;
+                curMin = Math.min(curMin, right);
+            }
+
+            res = Math.max(res, curMin * (r - l + 1));
+        }
+
+        return res;
+    }
+}
