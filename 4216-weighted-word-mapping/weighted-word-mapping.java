1fb6f1cf2e60985af0e7706c4654dffc19925840
Pavan Soni <pavansoni210129@gmail.com>
2026-06-13 10:45:52 +0530

    Time: 3 ms (33.62%) | Memory: 46.5 MB (65.52%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans=new StringBuilder();
+
+        for(String word:words){
+            int sum=0;
+            for(char ch:word.toCharArray()){
+                sum+=weights[ch- 'a'];
+            }
+            int val=sum%26;
+
+            char mapped=(char) ('z'-val);
+
+            ans.append(mapped);
+        }
+        return ans.toString();
+        
+    }
+}
