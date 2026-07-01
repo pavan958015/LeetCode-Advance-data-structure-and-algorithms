f20de2ae53a35f417a8d53c3e2958adf3e8aff09
Pavan Soni <pavansoni210129@gmail.com>
2026-07-02 04:48:13 +0530

    Time: 48 ms (5.48%) | Memory: 54 MB (50.77%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -3,23 +3,23 @@ class Solution {
         int n=text1.length();
         int m=text2.length();
 
-        int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<=n;i++)
-            Arrays.fill(dp[i],-1);
 
-        return solve(n,m,text1,text2,dp);
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return solve(0,0,n,m,text1,text2,dp);
     }
-    public static int solve(int idx1,int idx2,String s1,String s2,int[][] dp){
-        if(idx1<=0 || idx2<=0) return 0;
+    private int solve(int idx1,int idx2,int n,int m,String t,String s,int[][] dp){
+        if(idx1>=n || idx2>=m) return 0;
 
         if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
 
-        int pickup=0;
-        if(s1.charAt(idx1-1)==s2.charAt(idx2-1)){
-            pickup=1+solve(idx1-1,idx2-1,s1,s2,dp);
+        int pick=0;
+        if(t.charAt(idx1)==s.charAt(idx2)){
+            pick=1+solve(idx1+1,idx2+1,n,m,t,s,dp);
         }
-        int notPickup=Math.max(solve(idx1-1,idx2,s1,s2,dp),solve(idx1,idx2-1,s1,s2,dp));
-
-        return dp[idx1][idx2]=Math.max(pickup,notPickup);
+        int notPick=Math.max(solve(idx1+1,idx2,n,m,t,s,dp),solve(idx1,idx2+1,n,m,t,s,dp));
+        return dp[idx1][idx2]=Math.max(pick,notPick);
     }
 }
