60260f628b092a3beb410f3e149fb7943aca921c
Pavan Soni <pavansoni210129@gmail.com>
2026-07-10 21:34:46 +0530

    Time: 1 ms (100.00%) | Memory: 119.4 MB (64.79%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,33 +1,32 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // total gas
-        int totalgas=0;
-        for(int n:gas){
-            totalgas+=n;
-        }
+        int totalGas=0;
+        int totalCost=0;
 
+        for(int num:gas)
+            totalGas+=num;
+        for(int num:cost)
+            totalCost+=num;
 
-        // total cost
-        int totalcost=0;
-        for(int n:cost){
-            totalcost+=n;
-        }
-        // for -1 invalid case;
-        if(totalgas<totalcost) return -1;
+        // invalid condition
+        if(totalGas<totalCost) return -1;
 
+        int n=gas.length;
 
         int start=0;
-        int curGas=0;
+        int total=0;
 
-        for(int i=0;i<gas.length;i++){
-            curGas+=gas[i]-cost[i];
+        // we sure that it will give answer because for invalid we already checked
 
-            if(curGas<0){
+        for(int i=0;i<n;i++){
+            total+=gas[i]-cost[i];
+
+            if(total<0){
                 start=i+1;
-                curGas=0;
+                total=0;
             }
         }
         return start;
-
+        
     }
 }
