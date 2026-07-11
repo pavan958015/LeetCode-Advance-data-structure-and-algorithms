ef2956945b3123f3f32a617cb440244ef5a48eb9
Pavan Soni <pavansoni210129@gmail.com>
2026-07-11 18:35:56 +0530

    Time: 384 ms (5.00%) | Memory: 48.4 MB (9.07%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,28 +1,19 @@
-import java.util.*;
-
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        Integer[] dp = new Integer[n]; // 1 = true, 0 = false
-
-        return solve(0, n - 1, nums, dp);
+        Boolean dp[]=new Boolean[nums.length+1];
+        return solve(0,nums.length-1,dp,nums);
     }
+    private boolean solve(int idx,int n,Boolean[] dp,int[]nums){
+        if(idx>=n) return true;
 
-    public boolean solve(int idx, int n, int[] nums, Integer[] dp) {
-        if (idx >= n) return true;
+        if(dp[idx]!=null) return dp[idx]; 
 
-        if (dp[idx] != null) return dp[idx] == 1;
+        int jump=nums[idx];
 
-        int maxJump = nums[idx];
-
-        for (int i = 1; i <= maxJump; i++) {
-            if (idx + i <= n && solve(idx + i, n, nums, dp)) {
-                dp[idx] = 1;
-                return true;
-            }
+        for(int i=1;i<=jump;i++){
+            if(solve(idx+i,n,dp,nums)) return dp[idx+i]=true;
         }
 
-        dp[idx] = 0; // ❗ store false properly
-        return false;
+        return dp[idx]=false;
     }
 }
