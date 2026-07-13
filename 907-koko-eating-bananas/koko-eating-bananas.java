0e53e4c09d91770468045914221f7fdc8aa9670c
Pavan Soni <pavansoni210129@gmail.com>
2026-07-13 07:26:30 +0530

    Time: 6 ms (99.59%) | Memory: 47.6 MB (87.49%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,35 +1,45 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = 0;
-
-        for (int pile : piles) {
-            high = Math.max(high, pile);
-        }
-
-        int ans = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canFinish(piles, h, mid)) {
-                ans = mid;
-                high = mid - 1;   
-            } else {
-                low = mid + 1;    
+    private int findMax(int nums[]){
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            if(max<num){
+                max=num;
             }
         }
+        return max;
+    }
+    private boolean findValidity(int[] nums,int h,int perHour){
+        long totalHour=0;
+        for(int num:nums){
+            totalHour += (num + perHour - 1) / perHour;
+        }
+        return totalHour<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int max=findMax(piles);
 
+        // TLE
+
+        // for(int i=1;i<=max;i++){
+        //     int totalHours=findValidity(piles,i);
+        //     if(totalHours<=h) return i;
+        // }
+
+        int low=1; int high=max;
+        int ans=high;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(findValidity(piles,h,mid)){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
         return ans;
     }
 
-    private boolean canFinish(int[] piles, int h, int speed) {
-        long hours = 0;
-
-        for (int pile : piles) {
-            hours += (pile + speed - 1) / speed; 
-        }
-
-        return hours <= h;
-    }
 }
