2544859da2fd0d1bf6ca56b612b0e8106908c73d
Pavan Soni <pavansoni210129@gmail.com>
2026-07-14 22:51:40 +0530

    Time: 6 ms (84.56%) | Memory: 46.8 MB (56.68%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    TreeSet<Integer> set = new TreeSet<>();
+
+    public int[] findEvenNumbers(int[] digits) {
+
+        int[] freq = new int[10];
+
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        backtrack(freq, 0, 0);
+
+        int[] ans = new int[set.size()];
+        int i = 0;
+
+        for (int num : set) {
+            ans[i++] = num;
+        }
+
+        return ans;
+    }
+
+    private void backtrack(int[] freq, int len, int num) {
+
+        if (len == 3) {
+            if (num >= 100 && num % 2 == 0) {
+                set.add(num);
+            }
+            return;
+        }
+
+        for (int d = 0; d <= 9; d++) {
+
+            if (freq[d] == 0)
+                continue;
+
+            // First digit cannot be 0
+            if (len == 0 && d == 0)
+                continue;
+
+            freq[d]--;
+
+            backtrack(freq, len + 1, num * 10 + d);
+
+            freq[d]++;
+        }
+    }
+}
