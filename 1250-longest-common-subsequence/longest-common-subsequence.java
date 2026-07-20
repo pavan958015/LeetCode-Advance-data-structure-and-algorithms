95879cf27d934ef8cc4f13dc3bf63e3cc4de76ef
Pavan Soni <pavansoni210129@gmail.com>
2026-07-20 13:33:52 +0530

    Time: 55 ms (5.38%) | Memory: 75.2 MB (5.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,25 +1,23 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length();
-        int m=text2.length();
+        int m=text2.length(); 
 
+        Integer dp[][]=new Integer[n][m];
 
-        int dp[][]=new int[n+1][m+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
-        }
         return solve(0,0,n,m,text1,text2,dp);
     }
-    private int solve(int idx1,int idx2,int n,int m,String t,String s,int[][] dp){
+    private int solve(int idx1,int idx2,int n,int m,String s,String t,Integer[][] dp){
         if(idx1>=n || idx2>=m) return 0;
 
-        if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
+        if(dp[idx1][idx2]!=null) return dp[idx1][idx2];
 
-        int pick=0;
-        if(t.charAt(idx1)==s.charAt(idx2)){
-            pick=1+solve(idx1+1,idx2+1,n,m,t,s,dp);
+        int match=0;
+        if(s.charAt(idx1)==t.charAt(idx2)){
+            match=1+solve(idx1+1,idx2+1,n,m,s,t,dp);
         }
-        int notPick=Math.max(solve(idx1+1,idx2,n,m,t,s,dp),solve(idx1,idx2+1,n,m,t,s,dp));
-        return dp[idx1][idx2]=Math.max(pick,notPick);
+        int notMatch=Math.max(solve(idx1+1,idx2,n,m,s,t,dp),solve(idx1,idx2+1,n,m,s,t,dp));
+
+        return dp[idx1][idx2]=Math.max(match,notMatch);
     }
 }
