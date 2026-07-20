9215433c4edb1e38d2d75992cf9d0728ed439032
Pavan Soni <pavansoni210129@gmail.com>
2026-07-20 21:54:36 +0530

    Time: 34 ms (5.33%) | Memory: 49.7 MB (5.75%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,77 +1,36 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int dp[][]=new int[s1.length()+1][s2.length()+1];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return solve(0,0,s1,s2,dp);
+        int n=s1.length();
+        int m=s2.length();
+
+        Integer[][] dp=new Integer[n][m];
+
+        return solve(0,0,n,m,s1,s2,dp);   
     }
-    /*
-    public static int solve(int i,int j,String s1,String s2){
-        // base case;
-        if(i==s1.length() && j==s2.length()) return 0;
-
-        if(i==s1.length()){
-            int ans=0;
-            for(int k=j;k<s2.length();k++){
-                ans+=s2.charAt(k);
+    private int solve(int i,int j,int n, int m,String s,String t,Integer[][] dp){
+        if(i>=n){
+            int sum=0;
+            for(int x=j;x<m;x++){
+                sum+=t.charAt(x);
             }
-            return ans;
+            return sum;
         }
-
-        if(j==s2.length()){
-            int ans=0;
-            for(int k=i;k<s1.length();k++){
-                ans+=s1.charAt(k);
+        if(j>=m){
+            int sum=0;
+            for(int x=i;x<n;x++){
+                sum+=s.charAt(x);
             }
-            return ans;
-        }
-        
-
-
-
-        // recursion
-        if(s1.charAt(i)==s2.charAt(j)){
-            return solve(i+1,j+1,s1,s2);
-        }
-        // not matches
-        return Math.min(s1.charAt(i)+solve(i+1,j,s1,s2),
-                        s2.charAt(j)+solve(i,j+1,s1,s2));
-    }
-    */
-
-
-    public static int solve(int i,int j,String s1,String s2,int[][]dp){
-        // base case;
-        if(i==s1.length() && j==s2.length()) return 0;
-
-        if(i==s1.length()){
-            int ans=0;
-            for(int k=j;k<s2.length();k++){
-                ans+=s2.charAt(k);
-            }
-            return dp[i][j]=ans;
+            return sum;
         }
 
-        if(j==s2.length()){
-            int ans=0;
-            for(int k=i;k<s1.length();k++){
-                ans+=s1.charAt(k);
-            }
-            return dp[i][j]=ans;
-        }
-        
-        if(dp[i][j]!=-1) return dp[i][j];
+        if(dp[i][j]!=null) return dp[i][j];
 
-
-        // recursion
-        if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j]=solve(i+1,j+1,s1,s2,dp);
+        int same=Integer.MAX_VALUE;
+        if(s.charAt(i)==t.charAt(j)){
+            same=solve(i+1,j+1,n,m,s,t,dp);
         }
-        // not matches
-        return dp[i][j]= Math.min(s1.charAt(i)+solve(i+1,j,s1,s2,dp),
-                        s2.charAt(j)+solve(i,j+1,s1,s2,dp));
+
+        int notSame=Math.min(s.charAt(i)+solve(i+1,j,n,m,s,t,dp),t.charAt(j)+solve(i,j+1,n,m,s,t,dp));
+        return dp[i][j]=Math.min(same,notSame);
     }
 }
