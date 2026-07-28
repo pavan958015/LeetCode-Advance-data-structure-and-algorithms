8bdcd3725d9d3965ba46496d4ae217361e5471ee
Pavan Soni <pavansoni210129@gmail.com>
2026-07-29 05:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (17.35%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -1,11 +1,13 @@
 class Solution {
     public int trailingZeroes(int n) {
         if(n<5) return 0;
-        int res=0;
+        
+
+        int count=0;
         while(n>0){
             n/=5;
-            res+=n;
+            count+=n;
         }
-        return res;
+        return count;
     }
 }
