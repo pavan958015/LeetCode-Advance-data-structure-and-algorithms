8ee02975e86f6537a63bfebefb1d679a0eb4a6fd
Pavan Soni <pavansoni210129@gmail.com>
2026-07-31 05:27:07 +0530

    Time: 7 ms (53.98%) | Memory: 43.9 MB (63.89%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -2,22 +2,43 @@ class Solution {
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int dp[]=new int[arr.length];
         Arrays.fill(dp,-1);
-
-        return solve(0,arr,k,dp);       
+        // return solve(0,k,arr,dp);
+        return tabulation(arr,k);
     }
-    private int solve(int idx,int[] arr,int k,int[] dp){
+    public static int solve(int idx,int k,int[] arr,int[] dp){
         if(idx==arr.length) return 0;
 
         if(dp[idx]!=-1) return dp[idx];
-        int len=0;
-        int max=0;
+
+        int len=0,max=0;
         int maxSum=0;
         for(int i=idx;i<Math.min(arr.length,idx+k);i++){
             max=Math.max(max,arr[i]);
             len++;
-            int sum=len*max+solve(i+1,arr,k,dp);
+            int sum=len*max+solve(i+1,k,arr,dp);
             maxSum=Math.max(sum,maxSum);
         }
         return dp[idx]=maxSum;
+    }
+    public int tabulation(int[] arr, int k) {
+        int n = arr.length;
+        int[] dp = new int[n + 1];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            int len = 0, max = 0;
+            int maxSum = 0;
+
+            for (int i = idx; i < Math.min(n, idx + k); i++) {
+                max = Math.max(max, arr[i]);
+                len++;
+
+                int sum = len * max + dp[i + 1];
+                maxSum = Math.max(maxSum, sum);
+            }
+
+            dp[idx] = maxSum;
+        }
+
+        return dp[0];
     }
 }
