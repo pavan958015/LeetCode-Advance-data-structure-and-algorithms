67f89c0f476ea3fe42d517a42525362e7f0be62a
Pavan Soni <pavansoni210129@gmail.com>
2026-08-01 05:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (98.99%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,14 +1,17 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
         
-        return solve(0,nums.length-1,nums)>=0;
+        Integer[][] dp=new Integer[nums.length][nums.length];
+        return solve(0,nums.length-1,nums,dp)>=0;
     }
-    private int solve(int i,int j,int[] nums){
+    private int solve(int i,int j,int[] nums,Integer[][] dp){
         if(i==j) return nums[i];
 
-        int pickLeft=nums[i]-solve(i+1,j,nums);
-        int pickRight=nums[j]-solve(i,j-1,nums);
+        if(dp[i][j]!=null) return dp[i][j];
 
-        return Math.max(pickLeft,pickRight);
+        int pickLeft=nums[i]-solve(i+1,j,nums,dp);
+        int pickRight=nums[j]-solve(i,j-1,nums,dp);
+
+        return dp[i][j]= Math.max(pickLeft,pickRight);
     }
 }
