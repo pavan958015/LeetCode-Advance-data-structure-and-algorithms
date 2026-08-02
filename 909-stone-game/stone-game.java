4c3fe21dd4617e4ac099353de46189a1a23574ef
Pavan Soni <pavansoni210129@gmail.com>
2026-08-02 06:35:32 +0530

    Time: 11 ms (7.25%) | Memory: 49.7 MB (6.49%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,20 @@
 class Solution {
-    public boolean stoneGame(int[] piles) {
-        return true;
+    // public boolean stoneGame(int[] piles) {
+    //     return true;
+    // }
+    public boolean stoneGame(int[] piles){
+        Integer[][] dp=new Integer[piles.length][piles.length];
+
+        return solve(0,piles.length-1,piles,dp)>0;
+    }
+    private int solve(int i,int j,int[] piles,Integer[][] dp){
+        if(i==j) return piles[i];
+
+        if(dp[i][j]!=null) return dp[i][j];
+        int left=piles[i]-solve(i+1,j,piles,dp);
+        int right=piles[j]-solve(i,j-1,piles,dp);
+
+
+        return dp[i][j]=Math.max(left,right);
     }
 }
