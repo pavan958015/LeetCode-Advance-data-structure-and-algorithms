a3f08dd7065c874e55c43a2bcffdde89b0d3c0a6
Pavan Soni <pavansoni210129@gmail.com>
2026-08-05 16:52:24 +0530

    Time: 15 ms (65.13%) | Memory: 48 MB (11.97%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,62 +1,58 @@
 class Solution {
-    static class Triple{
+
+    static class Triple {
         int dist;
-        int second;
-        int third;
-        Triple(int d,int s,int t){
-            dist=d;
-            second=s;
-            third=t;
+        int r;
+        int c;
+
+        Triple(int dist, int r, int c) {
+            this.dist = dist;
+            this.r = r;
+            this.c = c;
         }
     }
+
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n=grid.length;
-        return shortestPath(grid,new int[]{0,0},new int[]{n-1,n-1});
-    }
-    private int shortestPath(int[][] grid, int[] source, int[] destination) {
 
-        if (grid[source[0]][source[1]] == 1 || grid[destination[0]][destination[1]] == 1)
+        int n = grid.length;
+
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1)
             return -1;
-        if(source[0]==destination[0] && source[1]==destination[1]) return 1;
-        Queue<Triple> q=new LinkedList<>();
-        int n=grid.length;
-        int m=grid[0].length;
-        
-        int dist[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                dist[i][j]=Integer.MAX_VALUE;
-            }
-        }
-        dist[source[0]][source[1]]=1;
-        q.add(new Triple(1,source[0],source[1]));
-        
-        
-        
-        int[] dx = {-1,-1,-1, 0,0, 1,1,1};
-        int[] dy = {-1, 0, 1,-1,1,-1,0,1};
 
-        while(!q.isEmpty()){
-            Triple cur=q.poll();
-            int dis=cur.dist;
-            int r=cur.second;
-            int c=cur.third;
-            for(int i=0;i<8;i++){
-                    int newR=r+dx[i];
-                    int newC=c+dy[i];
-                    
-                    if(newR>=0 && newR<n && newC>=0 && newC<m && grid[newR][newC]==0
-                               && dis+1<dist[newR][newC]){
-                                   dist[newR][newC]=cur.dist+1;
-                        if(newR==destination[0] && newC==destination[1]){
-                            return cur.dist+1;
-                        }
-                        q.add(new Triple(1+cur.dist,newR,newC));
-                    }
-                    
-                    
+        Queue<Triple> q = new LinkedList<>();
+        boolean[][] vis = new boolean[n][n];
+
+        int[] dRow = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dCol = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        q.offer(new Triple(1, 0, 0));
+        vis[0][0] = true;
+
+        while (!q.isEmpty()) {
+
+            Triple cur = q.poll();
+
+            int dist = cur.dist;
+            int r = cur.r;
+            int c = cur.c;
+
+            if (r == n - 1 && c == n - 1)
+                return dist;
+
+            for (int i = 0; i < 8; i++) {
+
+                int nr = r + dRow[i];
+                int nc = c + dCol[i];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n &&
+                    !vis[nr][nc] && grid[nr][nc] == 0) {
+
+                    vis[nr][nc] = true;
+                    q.offer(new Triple(dist + 1, nr, nc));
                 }
             }
-            return -1;
         }
+
+        return -1;
+    }
 }
