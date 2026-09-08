230f21054ce2630eb47ca4bf66884139fc712362
Pavan Soni <pavansoni210129@gmail.com>
2026-09-08 16:25:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (35.97%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,7 +2,8 @@ class Solution {
     public int climbStairs(int n){
         Integer[] dp=new Integer[n+1];
 
-        return solve(n,dp);
+        // return solve(n,dp);
+        return tabulation(n);
     }
     public int solve(int idx,Integer[] dp) {
         if(idx<=2) return idx;
@@ -10,5 +11,16 @@ class Solution {
         if(dp[idx]!=null) return dp[idx];
 
         return dp[idx]=solve(idx-1,dp)+solve(idx-2,dp);
+    }
+    private int tabulation(int n){
+        Integer[] dp=new Integer[n+1];
+
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
