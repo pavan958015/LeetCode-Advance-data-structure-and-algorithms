841b59b252bc5f74a776dc8fc843379097a9acbb
Pavan Soni <pavansoni210129@gmail.com>
2026-09-08 17:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (41.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,70 +1,28 @@
 class Solution {
-    public int rob(int[] nums){
+    public int rob(int[] nums) {
         int n=nums.length;
-        if(n==1) return nums[n-1];
+        if(n==1) return nums[0];
+        int skipFirst[]=new int[n-1];
+        int skipLast[]=new int[n-1];
 
-        
-        int temp1[]=new int[n-1];
-        int temp2[]=new int[n-1];
         for(int i=0;i<n-1;i++){
-            temp1[i]=nums[i+1];     // skiped First;
-            temp2[i]=nums[i];       // skip Last;
+            skipFirst[i]=nums[i+1];
+            skipLast[i]=nums[i];
         }
-        int dp1[]=new int[n-1];
-        int dp2[]=new int[n-1];
-        Arrays.fill(dp1,-1);
-        Arrays.fill(dp2,-1);
+        Integer[] dp1=new Integer[n-1];
+        Integer[] dp2=new Integer[n-1];
 
-        int first=helper(0,temp1,dp1);
-        int last=helper(0,temp2,dp2);
 
-        return Math.max(first,last);
+        return Math.max(solve(0,skipFirst,dp1),solve(0,skipLast,dp2));
     }
-    public static int helper(int idx,int[] arr,int dp[]){
-        if(idx>=arr.length) return 0;
+    private int solve(int idx,int[]nums,Integer[] dp){
+        if(idx>=nums.length) return 0;
 
-        if(dp[idx]!=-1) return dp[idx];
+        if(dp[idx]!=null) return dp[idx];
 
-        int take=arr[idx]+helper(idx+2,arr,dp);
+        int pick=nums[idx]+solve(idx+2,nums,dp);
+        int notPick=solve(idx+1,nums,dp);
 
-        int notTake=helper(idx+1,arr,dp);
-
-        return dp[idx]= Math.max(take,notTake);
+        return dp[idx]=Math.max(pick,notPick);
     }
-
-
-
-    // Space Optimization
-
-    // public int rob(int[] nums) {
-    //     int n=nums.length;
-    //     if(n==1){
-    //         return nums[n-1];
-    //     }
-    //     int temp1[]=new int[n-1];
-    //     int temp2[]=new int[n-1];
-
-    //     for (int i = 0; i < n - 1; i++) {
-    //         temp1[i] = nums[i + 1]; // skip first
-    //         temp2[i] = nums[i];     // skip last
-    //     }
-    //     return Math.max(helper(temp1),helper(temp2));
-
-    // }   
-    // public int helper(int[] nums){
-    //     int n=nums.length;
-    //     int prev=nums[0];
-    //     int prev2=0;
-    //     for(int i=1;i<n;i++){
-    //         int accept=nums[i];
-    //         if(i>1)
-    //             accept+=prev2;
-    //         int skip=prev;
-    //         int cur=Math.max(accept,skip);
-
-    //         prev2=prev;
-    //         prev=cur;
-    //     }
-    //     return prev;
-    // }
 }
