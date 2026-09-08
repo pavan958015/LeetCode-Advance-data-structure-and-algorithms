6aee87631bd29cc7cd1119a2a4bf7e2a0e31a47d
Pavan Soni <pavansoni210129@gmail.com>
2026-09-08 16:50:03 +0530

    Time: 1 ms (39.78%) | Memory: 46.4 MB (9.73%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -4,7 +4,9 @@ class Solution {
         int oneStep = solve(0, cost,dp);
         int twoStep = solve(1, cost,dp);
 
-        return Math.min(oneStep, twoStep);
+        // return Math.min(oneStep, twoStep);
+
+        return tabulation(cost);
     }
 
     private int solve(int idx, int[] nums,Integer[] dp) {
@@ -17,6 +19,18 @@ class Solution {
         int two = nums[idx] + solve(idx + 2, nums,dp);
 
         return dp[idx]=Math.min(one, two);
+    }
+    private int tabulation(int[] nums){
+        int n=nums.length;
+        Integer[] dp=new Integer[n];
 
+        if(n==1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+
+        for(int i=2;i<n;i++){
+            dp[i]=Math.min(nums[i]+dp[i-1],nums[i]+dp[i-2]);
+        }
+        return Math.min(dp[n-2],dp[n-1]);
     }
 }
