d63a152e12806e03268472902761792b432054af
Pavan Soni <pavansoni210129@gmail.com>
2026-09-08 16:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (5.84%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,8 @@
 class Solution {
     public int rob(int[] nums) {
         Integer[] dp=new Integer[nums.length+1];
-        return solve(0,nums,dp);
+        // return solve(0,nums,dp);
+        return tabulation(nums);
     }
     private int solve(int idx,int[] nums,Integer[] dp){
         if(idx>=nums.length) return 0;
@@ -13,5 +14,20 @@ class Solution {
         int notPick=solve(idx+1,nums,dp);
 
         return dp[idx]=Math.max(pick,notPick);
+    }
+
+    private int tabulation(int[] nums){
+        Integer[] dp=new Integer[nums.length];
+
+        if(nums.length==1) return nums[0];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            int pick=nums[i]+dp[i-2];
+            int notPick=dp[i-1];
+
+            dp[i]=Math.max(pick,notPick);
+        }
+        return dp[nums.length-1];
     }
 }
