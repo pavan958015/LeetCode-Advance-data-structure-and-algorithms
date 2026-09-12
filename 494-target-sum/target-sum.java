ce646baa52f6a6a67068b91346a0319a6bff220c
Pavan Soni <pavansoni210129@gmail.com>
2026-09-12 21:40:47 +0530

    Time: 624 ms (22.04%) | Memory: 42.9 MB (93.63%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,53 +1,16 @@
-// class Solution {
-//     public int findTargetSumWays(int[] nums, int target) {
-//         return solve(0, target, nums);
-//     }
-
-//     public static int solve(int idx, int target, int[] nums) {
-
-//         if (idx == nums.length) {
-//             return target == 0 ? 1 : 0;
-//         }
-
-//         int positive = solve(idx + 1, target - nums[idx], nums);
-//         int negative = solve(idx + 1, target + nums[idx], nums);
-
-//         return  positive + negative;
-//     }
-// }
-
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        if ((totalSum + target) % 2 != 0 || Math.abs(target) > totalSum) {
-            return 0;
-        }
-        
-        int s1 = (totalSum + target) / 2;
-        Integer[][] dp = new Integer[nums.length][s1 + 1];
-        return solve(0, s1, nums, dp);
+        return solve(0,nums,target);
     }
-    
-    private int solve(int idx, int currentTarget, int[] nums, Integer[][] dp) {
-        if (idx == nums.length) {
-            return currentTarget == 0 ? 1 : 0;
+    private int solve(int idx,int[] nums,int target){
+        if(idx==nums.length){
+            if(target==0) return 1;
+            else return 0;
         }
-        
-        if (dp[idx][currentTarget] != null) {
-            return dp[idx][currentTarget];
-        }
-        
-        int skip = solve(idx + 1, currentTarget, nums, dp);
-        
-        int include = 0;
-        if (nums[idx] <= currentTarget) {
-            include = solve(idx + 1, currentTarget - nums[idx], nums, dp);
-        }
-        
-        return dp[idx][currentTarget] = skip + include;
+
+        int positive=solve(idx+1,nums,target+nums[idx]);
+        int negative=solve(idx+1,nums,target-nums[idx]);
+
+        return positive+negative;
     }
 }
