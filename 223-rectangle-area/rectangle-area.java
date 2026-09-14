5fd18995255a3960dc7eaae97a3ea111780b1542
Pavan Soni <pavansoni210129@gmail.com>
2026-09-14 23:01:03 +0530

    Time: 2 ms (68.64%) | Memory: 44.3 MB (67.72%) - LeetSync

--- 223-rectangle-area/rectangle-area.java
+++ 223-rectangle-area/rectangle-area.java
@@ -1,19 +1,20 @@
 class Solution {
-    public int computeArea(int ax1, int ay1, int ax2, int ay2,
-                           int bx1, int by1, int bx2, int by2) {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        // firstly we can check that there is overlap or not 
+        // check question no 836
 
-        long area1 = (long)(ax2 - ax1) * (ay2 - ay1);
-        long area2 = (long)(bx2 - bx1) * (by2 - by1);
+        int area1=(ax2-ax1)*(ay2-ay1);
+        int area2=(bx2-bx1)*(by2-by1);
 
-        long overlapWidth = Math.min(ax2, bx2) - Math.max(ax1, bx1);
-        long overlapHeight = Math.min(ay2, by2) - Math.max(ay1, by1);
 
-        long overlap = 0;
+        int overlapWidth=Math.min(ax2,bx2)-Math.max(ax1,bx1);
+        int overlapHeight=Math.min(ay2,by2)-Math.max(ay1,by1);
 
-        if (overlapWidth > 0 && overlapHeight > 0) {
-            overlap = overlapWidth * overlapHeight;
+        int overlap=0;
+        if(overlapWidth> 0 && overlapHeight>0){
+            overlap=overlapWidth*overlapHeight;
         }
 
-        return (int)(area1 + area2 - overlap);
+        return area1+area2-overlap;
     }
 }
